4759b0d7d70a86ed12a0f9a2db12c1b1adacf298
stuarthillary <shillary.git@gmail.com>
2021-03-11 23:23:25 +0000

    VertexEarliestTimeThresholder cleanup

--- Sage/Graphs/VertexEarliestTimeThresholder.cs
+++ Sage/Graphs/VertexEarliestTimeThresholder.cs
@@ -1,24 +1,25 @@
 /* This source code licensed under the GNU Affero General Public License */
 
 
-using System;
-using _Debug = System.Diagnostics.Debug;
-using System.Collections;
 using Highpoint.Sage.SimCore; // For executive.
+using System;
+using System.Collections;
 
-namespace Highpoint.Sage.Graphs {
+namespace Highpoint.Sage.Graphs
+{
 
     /// <summary>
     /// When attached to a vertex in a graph, this object ensures that the vertex does not fire before a specified simulation time.
     /// </summary>
-    public class VertexEarliestTimeThresholder {
+    public class VertexEarliestTimeThresholder
+    {
 
         #region Private Fields
 
-        private TriggerDelegate m_vertexTrigger;
-        private IModel m_model;
-        private DateTime m_earliest;
-        private Vertex m_vertex;
+        private readonly TriggerDelegate _vertexTrigger;
+        private readonly IModel _model;
+        private DateTime _earliest;
+        private readonly Vertex _vertex;
 
         #endregion 
 
@@ -28,7 +29,7 @@ namespace Highpoint.Sage.Graphs {
         /// <param name="model">The model in which the <see cref="T:VertexEarliestTimeThresholder"/> exists.</param>
         /// <param name="vertex">The vertex that this object will control.</param>
 		public VertexEarliestTimeThresholder(IModel model, Vertex vertex)
-			:this(model,vertex,DateTime.MinValue){}
+            : this(model, vertex, DateTime.MinValue) { }
 
         /// <summary>
         /// Creates a new instance of the <see cref="T:VertexEarliestTimeThresholder"/> class.
@@ -36,35 +37,51 @@ namespace Highpoint.Sage.Graphs {
         /// <param name="model">The model in which the <see cref="T:VertexEarliestTimeThresholder"/> exists.</param>
         /// <param name="vertex">The vertex that this object will control.</param>
         /// <param name="earliest">The earliest time that the vertex should be allowed to fire.</param>
-		public VertexEarliestTimeThresholder(IModel model, Vertex vertex, DateTime earliest){
-			m_earliest = earliest;
-			m_model = model;
-			m_vertex = vertex;
-			m_vertexTrigger = vertex.FireVertex;
-			vertex.FireVertex = new TriggerDelegate(FireTheVertex);
-		}
+		public VertexEarliestTimeThresholder(IModel model, Vertex vertex, DateTime earliest)
+        {
+            _earliest = earliest;
+            _model = model;
+            _vertex = vertex;
+            _vertexTrigger = vertex.FireVertex;
+            vertex.FireVertex = new TriggerDelegate(FireTheVertex);
+        }
 
         /// <summary>
         /// Gets or sets the earliest time that the vertex should be allowed to fire.
         /// </summary>
         /// <value>The earliest time that the vertex should be allowed to fire.</value>
-		public DateTime Earliest { get { return m_earliest; } set { m_earliest = value; } }
+		public DateTime Earliest
+        {
+            get
+            {
+                return _earliest;
+            }
+            set
+            {
+                _earliest = value;
+            }
+        }
 
-		private void FireTheVertex(IDictionary graphContext){
-			if ( m_model.Executive.Now < m_earliest ) {
-				TimeSpan ts = (m_earliest-m_model.Executive.Now);
-				// _Debug.WriteLine(m_model.Executive.Now + " : " + "Will fire vertex " + m_vertex.Name + " after a delay of " + string.Format("{0:d2}:{1:d2}:{2:d2}",ts.Hours,ts.Minutes,ts.Seconds));
-				ExecEventType eet = m_model.Executive.CurrentEventType;
-				m_model.Executive.RequestEvent(new ExecEventReceiver(_FireTheVertex),m_earliest,0,graphContext,eet);
-			} else {
-				// _Debug.WriteLine(m_model.Executive.Now + " : " + "Firing vertex " + m_vertex.Name);
-				m_vertexTrigger(graphContext);
-			}
-		}
+        private void FireTheVertex(IDictionary graphContext)
+        {
+            if (_model.Executive.Now < _earliest)
+            {
+                TimeSpan ts = (_earliest - _model.Executive.Now);
+                // _Debug.WriteLine(m_model.Executive.Now + " : " + "Will fire vertex " + m_vertex.Name + " after a delay of " + string.Format("{0:d2}:{1:d2}:{2:d2}",ts.Hours,ts.Minutes,ts.Seconds));
+                ExecEventType eet = _model.Executive.CurrentEventType;
+                _model.Executive.RequestEvent(new ExecEventReceiver(_FireTheVertex), _earliest, 0, graphContext, eet);
+            }
+            else
+            {
+                // _Debug.WriteLine(m_model.Executive.Now + " : " + "Firing vertex " + m_vertex.Name);
+                _vertexTrigger(graphContext);
+            }
+        }
 
-		private void _FireTheVertex(IExecutive exec, object graphContext){
-			FireTheVertex((IDictionary)graphContext);
-		}
+        private void _FireTheVertex(IExecutive exec, object graphContext)
+        {
+            FireTheVertex((IDictionary)graphContext);
+        }
 
-	}
+    }
 }
