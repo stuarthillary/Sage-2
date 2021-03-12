2622459fda0885b51b5ff4b200dc91b834a96633
stuarthillary <shillary.git@gmail.com>
2021-03-12 01:01:46 +0000

    TaskManagementService cleanup
    Move interface out of TaskManagementService.cs. Cleanup items

---
+++ Sage/Graphs/Tasks/ITaskManagementService.cs
@@ -0,0 +1,56 @@
+﻿/* This source code licensed under the GNU Affero General Public License */
+using Highpoint.Sage.SimCore;
+using System;
+using System.Collections;
+
+namespace Highpoint.Sage.Graphs.Tasks
+{
+    public interface ITaskManagementService : IModelService
+    {
+        /// <summary>
+        /// Fired when a TaskProcessor is added to this model.
+        /// </summary>
+        event TaskProcessorListener TaskProcessorAddedEvent;
+
+        /// <summary>
+        /// Fired when a TaskProcessor is removed from this model.
+        /// </summary>
+        event TaskProcessorListener TaskProcessorRemovedEvent;
+
+        /// <summary>
+        /// Adds a task processor to this model. A Task Processor is an entity that knows when to
+        /// start executing a given task graph. This method must be called before the model starts.
+        /// </summary>
+        /// <param name="taskProcessor">The task processor being added to this model.</param>
+        void AddTaskProcessor(TaskProcessor taskProcessor);
+
+        /// <summary>
+        /// Removes a task processor from this model. This method must be called before the model starts.
+        /// </summary>
+        /// <param name="taskProcessor">The task processor being removed from this model.</param>
+        void RemoveTaskProcessor(TaskProcessor taskProcessor);
+
+        /// <summary>
+        /// The collection of task processors being managed by this model.
+        /// </summary>
+        ArrayList TaskProcessors
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Locates a task processor by its Guid.
+        /// </summary>
+        /// <param name="guid">The Guid of the task processor to be located.</param>
+        /// <returns>The task processor, if found, otherwise null.</returns>
+        TaskProcessor GetTaskProcessor(Guid guid);
+
+        /// <summary>
+        /// Returns the tasks known to this model.
+        /// </summary>
+        /// <param name="masterTasksOnly">If this is true, then only the root (master) tasks of all of the
+        /// known task graphs are returned. Otherwise, all tasks under those root tasks are included as well.</param>
+        /// <returns>A collection of the requested tasks.</returns>
+        ICollection GetTasks(bool masterTasksOnly);
+    }
+}
--- Sage/Graphs/Tasks/TaskManagementService.cs
+++ Sage/Graphs/Tasks/TaskManagementService.cs
@@ -1,78 +1,33 @@
 ﻿/* This source code licensed under the GNU Affero General Public License */
+using Highpoint.Sage.SimCore;
 using System;
 using System.Collections;
 using System.Diagnostics;
-using Highpoint.Sage.SimCore;
 using _Debug = System.Diagnostics.Debug;
 
 namespace Highpoint.Sage.Graphs.Tasks
 {
-
-    public interface ITaskManagementService : IModelService
-    {
-        /// <summary>
-        /// Fired when a TaskProcessor is added to this model.
-        /// </summary>
-        event TaskProcessorListener TaskProcessorAddedEvent;
-
-        /// <summary>
-        /// Fired when a TaskProcessor is removed from this model.
-        /// </summary>
-        event TaskProcessorListener TaskProcessorRemovedEvent;
-
-        /// <summary>
-        /// Adds a task processor to this model. A Task Processor is an entity that knows when to
-        /// start executing a given task graph. This method must be called before the model starts.
-        /// </summary>
-        /// <param name="taskProcessor">The task processor being added to this model.</param>
-        void AddTaskProcessor(TaskProcessor taskProcessor);
-
-        /// <summary>
-        /// Removes a task processor from this model. This method must be called before the model starts.
-        /// </summary>
-        /// <param name="taskProcessor">The task processor being removed from this model.</param>
-        void RemoveTaskProcessor(TaskProcessor taskProcessor);
-
-        /// <summary>
-        /// The collection of task processors being managed by this model.
-        /// </summary>
-        ArrayList TaskProcessors { get; }
-
-        /// <summary>
-        /// Locates a task processor by its Guid.
-        /// </summary>
-        /// <param name="guid">The Guid of the task processor to be located.</param>
-        /// <returns>The task processor, if found, otherwise null.</returns>
-        TaskProcessor GetTaskProcessor(Guid guid);
-
-        /// <summary>
-        /// Returns the tasks known to this model.
-        /// </summary>
-        /// <param name="masterTasksOnly">If this is true, then only the root (master) tasks of all of the
-        /// known task graphs are returned. Otherwise, all tasks under those root tasks are included as well.</param>
-        /// <returns>A collection of the requested tasks.</returns>
-        ICollection GetTasks(bool masterTasksOnly);
-    }
-
     public class TaskManagementService : ITaskManagementService
     {
-        private static readonly bool s_diagnostics = Diagnostics.DiagnosticAids.Diagnostics("TaskManagementService");
-        private static readonly bool s_managePostMortemData = Diagnostics.DiagnosticAids.Diagnostics("Graph.KeepPostMortems");
+        private static readonly bool _diagnostics = Diagnostics.DiagnosticAids.Diagnostics("TaskManagementService");
+        private static readonly bool _managePostMortemData = Diagnostics.DiagnosticAids.Diagnostics("Graph.KeepPostMortems");
 
-        private readonly Hashtable m_taskProcessors;
-        private IModel m_model;
+        private readonly Hashtable _taskProcessors;
+        private IModel _model;
         public TaskManagementService()
         {
-            m_taskProcessors = new Hashtable();
+            _taskProcessors = new Hashtable();
         }
 
-        
+
         #region >>> TaskProcessor Management <<<
 
-        public void OnModelStarting(IModel model){
+        public void OnModelStarting(IModel model)
+        {
             // A part of the protocol for this generic model is that 
             // all TaskProcessors run when the model runs.
-            foreach ( TaskProcessor tp in TaskProcessors ) tp.Activate();
+            foreach (TaskProcessor tp in TaskProcessors)
+                tp.Activate();
         }
 
         /// <summary>
@@ -89,15 +44,17 @@ namespace Highpoint.Sage.Graphs.Tasks
         /// start executing a given task graph. This method must be called before the model starts.
         /// </summary>
         /// <param name="taskProcessor">The task processor being added to this model.</param>
-        public void AddTaskProcessor(TaskProcessor taskProcessor){
+        public void AddTaskProcessor(TaskProcessor taskProcessor)
+        {
             // TODO: Add this to an Errors & Warnings collection instead of dumping it to Trace.
-            if ( m_taskProcessors.Contains(taskProcessor.Guid) ) {
+            if (_taskProcessors.Contains(taskProcessor.Guid))
+            {
                 _Debug.WriteLine("Model already contains task processor being added at:");
                 _Debug.WriteLine((new StackTrace()).ToString());
                 _Debug.WriteLine("...the request to add it will be ignored.");
                 return;
             }
-            m_taskProcessors.Add(taskProcessor.Guid,taskProcessor);
+            _taskProcessors.Add(taskProcessor.Guid, taskProcessor);
             TaskProcessorAddedEvent?.Invoke(this, taskProcessor);
         }
 
@@ -105,17 +62,20 @@ namespace Highpoint.Sage.Graphs.Tasks
         /// Removes a task processor from this model. This method must be called before the model starts.
         /// </summary>
         /// <param name="taskProcessor">The task processor being removed from this model.</param>
-        public void RemoveTaskProcessor(TaskProcessor taskProcessor) {
-            m_taskProcessors.Remove(taskProcessor.Guid);
+        public void RemoveTaskProcessor(TaskProcessor taskProcessor)
+        {
+            _taskProcessors.Remove(taskProcessor.Guid);
             TaskProcessorRemovedEvent?.Invoke(this, taskProcessor);
         }
 
         /// <summary>
         /// The collection of task processors being managed by this model.
         /// </summary>
-        public ArrayList TaskProcessors {
-            get {
-                ArrayList tps = new ArrayList(m_taskProcessors.Values);
+        public ArrayList TaskProcessors
+        {
+            get
+            {
+                ArrayList tps = new ArrayList(_taskProcessors.Values);
                 return ArrayList.ReadOnly(tps);
             }
         }
@@ -125,8 +85,9 @@ namespace Highpoint.Sage.Graphs.Tasks
         /// </summary>
         /// <param name="guid">The Guid of the task processor to be located.</param>
         /// <returns>The task processor, if found, otherwise null.</returns>
-        public TaskProcessor GetTaskProcessor(Guid guid){
-            return (TaskProcessor)m_taskProcessors[guid];
+        public TaskProcessor GetTaskProcessor(Guid guid)
+        {
+            return (TaskProcessor)_taskProcessors[guid];
         }
 
         /// <summary>
@@ -135,11 +96,14 @@ namespace Highpoint.Sage.Graphs.Tasks
         /// <param name="masterTasksOnly">If this is true, then only the root (master) tasks of all of the
         /// known task graphs are returned. Otherwise, all tasks under those root tasks are included as well.</param>
         /// <returns>A collection of the requested tasks.</returns>
-        public virtual ICollection GetTasks(bool masterTasksOnly){
+        public virtual ICollection GetTasks(bool masterTasksOnly)
+        {
             ArrayList kids = new ArrayList();
-            foreach ( TaskProcessor tp in TaskProcessors ) {
+            foreach (TaskProcessor tp in TaskProcessors)
+            {
                 kids.Add(tp.MasterTask);
-                if ( !masterTasksOnly ) kids.AddRange(tp.MasterTask.GetChildTasks(false));
+                if (!masterTasksOnly)
+                    kids.AddRange(tp.MasterTask.GetChildTasks(false));
             }
             return kids;
         }
@@ -153,13 +117,14 @@ namespace Highpoint.Sage.Graphs.Tasks
 
             ArrayList allTasks = new ArrayList();
 
-            foreach (TaskProcessor tp in m_taskProcessors.Values)
+            foreach (TaskProcessor tp in _taskProcessors.Values)
             {
-                if (allTasks.Contains(tp.MasterTask)) continue;
+                if (allTasks.Contains(tp.MasterTask))
+                    continue;
                 allTasks.AddRange(tp.MasterTask.GetChildTasks(false));
             }
 
-            if (s_diagnostics)
+            if (_diagnostics)
             {
                 _Debug.WriteLine("Clearing orphaned errors:\r\nKnown Tasks:");
                 foreach (Task task in allTasks)
@@ -169,38 +134,43 @@ namespace Highpoint.Sage.Graphs.Tasks
             }
 
             ArrayList keysToClear = new ArrayList();
-            foreach (IModelError err in m_model.Errors)
+            foreach (IModelError err in _model.Errors)
             {
                 if (err.Target is Task)
                 {
-                    if (s_diagnostics)
+                    if (_diagnostics)
                         _Debug.WriteLine("Checking error " + err.Narrative + ", targeted to " + ((Task)err.Target).Name);
                     if (!allTasks.Contains(err.Target))
                     {
-                        if (s_diagnostics)
+                        if (_diagnostics)
                             _Debug.WriteLine("Clearing error " + err.Name);
                         keysToClear.Add(err.Target);
                     }
                 }
             }
 
-            foreach (object key in keysToClear) m_model.ClearAllErrorsFor(key);
+            foreach (object key in keysToClear)
+                _model.ClearAllErrorsFor(key);
         }
 
-                /// <summary>
+        /// <summary>
         /// Returns the post mortem data on all known task graph executions. This data indicates the vertices
         /// and edges that fired and those that did not fire. It is typically fed into the Diagnostics class'
         /// DumpPostMortemData(...) API.
         /// </summary>
         /// <returns>A Hashtable of postmortem data.</returns>
-        public Hashtable GetPostMortems(){
+        public Hashtable GetPostMortems()
+        {
             Hashtable postmortems = new Hashtable();
 #if DEBUG
-            if ( s_managePostMortemData ) {
-                foreach ( TaskProcessor tp in TaskProcessors ) {
-                    foreach ( IDictionary graphContext in tp.GraphContexts ) {
+            if (_managePostMortemData)
+            {
+                foreach (TaskProcessor tp in TaskProcessors)
+                {
+                    foreach (IDictionary graphContext in tp.GraphContexts)
+                    {
                         PmData pmData = (PmData)graphContext["PostMortemData"];
-                        postmortems.Add(tp.Name,pmData);
+                        postmortems.Add(tp.Name, pmData);
                     }
                 }
             }
@@ -213,13 +183,19 @@ namespace Highpoint.Sage.Graphs.Tasks
 
         public void InitializeService(IModel model)
         {
-            m_model = model;
-            m_model.Starting += OnModelStarting;
+            _model = model;
+            _model.Starting += OnModelStarting;
         }
 
-        public bool IsInitialized {
-            get { return m_model != null; }
-            set { }
+        public bool IsInitialized
+        {
+            get
+            {
+                return _model != null;
+            }
+            set
+            {
+            }
         }
         public bool InlineInitialization => true;
     }
