c7ea51fdd9c2a8abf7456929788780484927ddb0
stuarthillary <shillary.git@gmail.com>
2021-03-12 01:03:47 +0000

    TaskProcessor cleanup

--- Sage/Graphs/Tasks/TaskProcessor.cs
+++ Sage/Graphs/Tasks/TaskProcessor.cs
@@ -1,12 +1,13 @@
 /* This source code licensed under the GNU Affero General Public License */
 
-using System;
-using _Debug = System.Diagnostics.Debug;
-using System.Collections;
-using Highpoint.Sage.SimCore;
 using Highpoint.Sage.Persistence;
+using Highpoint.Sage.SimCore;
+using System;
+using System.Collections;
+using _Debug = System.Diagnostics.Debug;
 
-namespace Highpoint.Sage.Graphs.Tasks {
+namespace Highpoint.Sage.Graphs.Tasks
+{
 
     /// <summary>
     /// TaskProcessor encapsulates a task, and is responsible for scheduling its
@@ -15,44 +16,46 @@ namespace Highpoint.Sage.Graphs.Tasks {
     /// task to be scheduled, and subsequently run - the default Model implementation
     /// does this automatically in the Running state method.
     /// </summary>
-    public class TaskProcessor : IModelObject, IXmlPersistable {
+    public class TaskProcessor : IModelObject, IXmlPersistable
+    {
 
-		#region Private Fields
-		private Task m_masterTask;
+        #region Private Fields
+        private Task _masterTask;
 
-		private bool m_startConditionsSpecified = false;
-		private DateTime m_when;
-		private double m_priority;
-		private ExecEventType m_eet;
-		private IModel m_model;
-		private string m_description = null;
-		private Guid m_guid = Guid.Empty;
-		private string m_name = null;
-		private bool m_keepGraphContexts = false;
+        private bool _startConditionsSpecified = false;
+        private DateTime _when;
+        private double _priority;
+        private ExecEventType _eet;
+        private IModel _model;
+        private string _description = null;
+        private Guid _guid = Guid.Empty;
+        private string _name = null;
+        private bool _keepGraphContexts = false;
 
-		private static readonly bool s_diagnostics = Diagnostics.DiagnosticAids.Diagnostics("TaskProcessor");
-		#endregion
-		
-		#region Protected Fields
-		protected IDictionary GraphContext;
-		protected ArrayList m_graphContexts = new ArrayList();
-		#endregion
+        private static readonly bool _diagnostics = Diagnostics.DiagnosticAids.Diagnostics("TaskProcessor");
+        #endregion
 
-		#region Constructors
-		public TaskProcessor(IModel model, string name, Task task):this(model,name,Guid.NewGuid(),task) {}
+        #region Protected Fields
+        protected IDictionary GraphContext;
+        protected ArrayList _graphContexts = new ArrayList();
+        #endregion
 
-		public TaskProcessor(IModel model, string name, Guid guid, Task task){
+        #region Constructors
+        public TaskProcessor(IModel model, string name, Task task) : this(model, name, Guid.NewGuid(), task) { }
+
+        public TaskProcessor(IModel model, string name, Guid guid, Task task)
+        {
             InitializeIdentity(model, name, null, guid);
-            
-            m_masterTask = task;
-			m_priority = 0.0;
-			m_when = DateTime.MinValue;
-			m_eet = ExecEventType.Synchronous;
+
+            _masterTask = task;
+            _priority = 0.0;
+            _when = DateTime.MinValue;
+            _eet = ExecEventType.Synchronous;
             Model.GetService<ITaskManagementService>().AddTaskProcessor(this);
-            
+
             IMOHelper.RegisterWithModel(this);
-		}
-		#endregion
+        }
+        #endregion
 
         /// <summary>
         /// Initialize the identity of this model object, once.
@@ -61,120 +64,181 @@ namespace Highpoint.Sage.Graphs.Tasks {
         /// <param name="name">The name of this component.</param>
         /// <param name="description">The description for this component.</param>
         /// <param name="guid">The GUID of this component.</param>
-        public void InitializeIdentity(IModel model, string name, string description, Guid guid) {
-            IMOHelper.Initialize(ref m_model, model, ref m_name, name, ref m_description, description, ref m_guid, guid);
+        public void InitializeIdentity(IModel model, string name, string description, Guid guid)
+        {
+            IMOHelper.Initialize(ref _model, model, ref _name, name, ref _description, description, ref _guid, guid);
         }
 
         /// <summary>
         /// Gets the master task for this Task Processor - the task that holds the root level of the task graph to be performed.
         /// </summary>
         /// <value>The master task.</value>
-        public Task MasterTask {
-            get {
-                return m_masterTask;
+        public Task MasterTask
+        {
+            get
+            {
+                return _masterTask;
             }
         }
 
-		public void SetConfigData(DateTime when, ExecEventType eet, double priority){
-			SetStartTime(when);
-			SetStartEventType(eet);
-			SetStartEventPriority(priority);
-		}
-
-        public void SetStartTime(DateTime when){
-            m_startConditionsSpecified = true;
-            m_when = when;
+        public void SetConfigData(DateTime when, ExecEventType eet, double priority)
+        {
+            SetStartTime(when);
+            SetStartEventType(eet);
+            SetStartEventPriority(priority);
         }
 
-		public DateTime StartTime { 
-			get { return m_when; }
-		}
-
-        public void SetStartEventType(ExecEventType eet){
-            m_eet = eet;
+        public void SetStartTime(DateTime when)
+        {
+            _startConditionsSpecified = true;
+            _when = when;
         }
 
-		public void SetStartEventPriority(double priority){
-			m_priority = priority;
-		}
+        public DateTime StartTime
+        {
+            get
+            {
+                return _when;
+            }
+        }
 
-        public virtual void Activate(){
+        public void SetStartEventType(ExecEventType eet)
+        {
+            _eet = eet;
+        }
+
+        public void SetStartEventPriority(double priority)
+        {
+            _priority = priority;
+        }
+
+        public virtual void Activate()
+        {
             //_Debug.WriteLine("Activating " + m_name );
-            if (!m_startConditionsSpecified){
-                m_when = m_model.Executive.Now;
-                m_priority = 0.0;
+            if (!_startConditionsSpecified)
+            {
+                _when = _model.Executive.Now;
+                _priority = 0.0;
             }
-			if ( GraphContext == null ) {
-				GraphContext = new Hashtable();
-			} else {
-				m_model.Executive.ClearVolatiles(GraphContext);
-			}
-            if ( m_keepGraphContexts ) m_graphContexts.Add(GraphContext);
-            m_model.Executive.RequestEvent(new ExecEventReceiver(BeginExecution),m_when,m_priority,GraphContext,m_eet);
+            if (GraphContext == null)
+            {
+                GraphContext = new Hashtable();
+            }
+            else
+            {
+                _model.Executive.ClearVolatiles(GraphContext);
+            }
+            if (_keepGraphContexts)
+                _graphContexts.Add(GraphContext);
+            _model.Executive.RequestEvent(new ExecEventReceiver(BeginExecution), _when, _priority, GraphContext, _eet);
         }
 
-        private void BeginExecution(IExecutive exec, object userData){
-			if ( s_diagnostics ) {
-				_Debug.WriteLine("Task processor " + Name + " beginning execution instance of graph " + m_masterTask.Name);
-			}
-            m_masterTask.Start((IDictionary)userData);
+        private void BeginExecution(IExecutive exec, object userData)
+        {
+            if (_diagnostics)
+            {
+                _Debug.WriteLine("Task processor " + Name + " beginning execution instance of graph " + _masterTask.Name);
+            }
+            _masterTask.Start((IDictionary)userData);
         }
 
-        public bool KeepGraphContexts { get { return m_keepGraphContexts; } set { m_keepGraphContexts = value; } }
-        public ArrayList GraphContexts { get { return ArrayList.ReadOnly(m_graphContexts); } }
-		public IDictionary CurrentGraphContext { get { return GraphContext; } }
+        public bool KeepGraphContexts
+        {
+            get
+            {
+                return _keepGraphContexts;
+            }
+            set
+            {
+                _keepGraphContexts = value;
+            }
+        }
+        public ArrayList GraphContexts
+        {
+            get
+            {
+                return ArrayList.ReadOnly(_graphContexts);
+            }
+        }
+        public IDictionary CurrentGraphContext
+        {
+            get
+            {
+                return GraphContext;
+            }
+        }
 
-        public string Name { get { return m_name; } }
-		/// <summary>
-		/// A description of this TaskProcessor.
-		/// </summary>
-		public string Description {
-			get { return m_description==null?m_name:m_description; }
-		}
-		public Guid Guid { 
-			get { return m_guid; } 
-			set { 
-				if ( m_model != null ) {
-					m_model.ModelObjects.Remove(m_guid);
-					m_model.ModelObjects.Add(value,this);
-				}
-				m_guid = value;
-			} 
-		}
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+        }
+        /// <summary>
+        /// A description of this TaskProcessor.
+        /// </summary>
+        public string Description
+        {
+            get
+            {
+                return _description ?? _name;
+            }
+        }
+        public Guid Guid
+        {
+            get
+            {
+                return _guid;
+            }
+            set
+            {
+                if (_model != null)
+                {
+                    _model.ModelObjects.Remove(_guid);
+                    _model.ModelObjects.Add(value, this);
+                }
+                _guid = value;
+            }
+        }
         /// <summary>
         /// The model that owns this object, or from which this object gets time, etc. data.
         /// </summary>
         /// <value>The model.</value>
-        public IModel Model => m_model;
+        public IModel Model => _model;
 
-		#region >>> Implementation of IXmlPersistable <<<
-		/// <summary>
-		/// Default constructor for serialization only.
-		/// </summary>
-		public TaskProcessor(){}
+        #region >>> Implementation of IXmlPersistable <<<
+        /// <summary>
+        /// Default constructor for serialization only.
+        /// </summary>
+        public TaskProcessor()
+        {
+        }
 
-		public virtual void SerializeTo(XmlSerializationContext xmlsc){
-			xmlsc.StoreObject("ExecEventType",m_eet);
-			xmlsc.StoreObject("Guid",m_guid);
-			xmlsc.StoreObject("KeepGCs",m_keepGraphContexts);
-			xmlsc.StoreObject("MasterTask",m_masterTask);
-			xmlsc.StoreObject("Name",m_name);
-			xmlsc.StoreObject("StartCondSpec",m_startConditionsSpecified);
-			xmlsc.StoreObject("When",m_when);
-		}
+        public virtual void SerializeTo(XmlSerializationContext xmlsc)
+        {
+            xmlsc.StoreObject("ExecEventType", _eet);
+            xmlsc.StoreObject("Guid", _guid);
+            xmlsc.StoreObject("KeepGCs", _keepGraphContexts);
+            xmlsc.StoreObject("MasterTask", _masterTask);
+            xmlsc.StoreObject("Name", _name);
+            xmlsc.StoreObject("StartCondSpec", _startConditionsSpecified);
+            xmlsc.StoreObject("When", _when);
+        }
 
-		public virtual void DeserializeFrom(XmlSerializationContext xmlsc){
-			m_model = (Model)xmlsc.ContextEntities["Model"];
-			m_eet = (ExecEventType)xmlsc.LoadObject("ExecEventType");
-			m_guid = (Guid)xmlsc.LoadObject("Guid");
-			m_keepGraphContexts = (bool)xmlsc.LoadObject("KeepGCs");
-			m_masterTask = (Task)xmlsc.LoadObject("MasterTask");
-			m_name = (string)xmlsc.LoadObject("Name");
-			m_startConditionsSpecified = (bool)xmlsc.LoadObject("StartCondSpec");
-			m_when = (DateTime)xmlsc.LoadObject("When");
+        public virtual void DeserializeFrom(XmlSerializationContext xmlsc)
+        {
+            _model = (Model)xmlsc.ContextEntities["Model"];
+            _eet = (ExecEventType)xmlsc.LoadObject("ExecEventType");
+            _guid = (Guid)xmlsc.LoadObject("Guid");
+            _keepGraphContexts = (bool)xmlsc.LoadObject("KeepGCs");
+            _masterTask = (Task)xmlsc.LoadObject("MasterTask");
+            _name = (string)xmlsc.LoadObject("Name");
+            _startConditionsSpecified = (bool)xmlsc.LoadObject("StartCondSpec");
+            _when = (DateTime)xmlsc.LoadObject("When");
 
-		}
+        }
 
-		#endregion
-	}
+        #endregion
+    }
 }
