b3688b999b2ae626579e832de9a23d45ea6646d8
stuarthillary <shillary.git@gmail.com>
2021-03-13 22:10:54 +0000

    SimpleDelay cleanup

--- Sage/Graphs/PFC/Execution/Actions/SimpleDelay.cs
+++ Sage/Graphs/PFC/Execution/Actions/SimpleDelay.cs
@@ -1,33 +1,41 @@
 /* This source code licensed under the GNU Affero General Public License */
-using System;
 using Highpoint.Sage.Mathematics;
 using Highpoint.Sage.SimCore;
+using System;
 
-namespace Highpoint.Sage.Graphs.PFC.Execution.Actions {
-    public class SimpleDelay : PfcActor {
+namespace Highpoint.Sage.Graphs.PFC.Execution.Actions
+{
+    public class SimpleDelay : PfcActor
+    {
 
-        private ITimeSpanDistribution m_tsd;
+        private readonly ITimeSpanDistribution _tsd;
 
         public SimpleDelay(IModel model, string name, Guid guid, IPfcStepNode myStepNode, ITimeSpanDistribution tsd)
-        :base(model,name,guid,myStepNode){
-            m_tsd = tsd;
+        : base(model, name, guid, myStepNode)
+        {
+            _tsd = tsd;
         }
 
-        public override void GetPermissionToStart(PfcExecutionContext myPfcec, StepStateMachine ssm) { }
+        public override void GetPermissionToStart(PfcExecutionContext myPfcec, StepStateMachine ssm)
+        {
+        }
 
-        public override void Run(PfcExecutionContext pfcec, StepStateMachine ssm) {
+        public override void Run(PfcExecutionContext pfcec, StepStateMachine ssm)
+        {
             IExecutive exec = Model.Executive;
-            exec.CurrentEventController.SuspendUntil(exec.Now + m_tsd.GetNext());
+            exec.CurrentEventController.SuspendUntil(exec.Now + _tsd.GetNext());
         }
 
 
-        public override void SetStochasticMode(StochasticMode mode) {
-            switch (mode) {
+        public override void SetStochasticMode(StochasticMode mode)
+        {
+            switch (mode)
+            {
                 case StochasticMode.Full:
-                    m_tsd.SetCDFInterval(0.0,1.0);
+                    _tsd.SetCDFInterval(0.0, 1.0);
                     break;
                 case StochasticMode.Schedule:
-                    m_tsd.SetCDFInterval(0.5,0.5);
+                    _tsd.SetCDFInterval(0.5, 0.5);
                     break;
                 default:
                     throw new ApplicationException("Unknown stochastic mode " + mode);
