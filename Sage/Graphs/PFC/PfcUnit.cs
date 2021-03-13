2b78dad72ab555f2ad1e60643ebf197b84f70f7f
stuarthillary <shillary.git@gmail.com>
2021-03-13 01:34:42 +0000

    PfcUnitInfo cleanup

--- Sage/Graphs/PFC/PfcUnit.cs
+++ Sage/Graphs/PFC/PfcUnit.cs
@@ -1,37 +1,46 @@
 /* This source code licensed under the GNU Affero General Public License */
 
-namespace Highpoint.Sage.Graphs.PFC {
-    public class PfcUnitInfo : IPfcUnitInfo {
+namespace Highpoint.Sage.Graphs.PFC
+{
+    public class PfcUnitInfo : IPfcUnitInfo
+    {
 
         #region Private Fields
-        private string m_name;
-        private int m_sequenceNumber;
+        private string _name;
+        private int _sequenceNumber;
         #endregion 
 
         #region Constructors
-        public PfcUnitInfo(string name, int sequenceNumber) {
-            m_name = name;
-            m_sequenceNumber = sequenceNumber;
+        public PfcUnitInfo(string name, int sequenceNumber)
+        {
+            _name = name;
+            _sequenceNumber = sequenceNumber;
         }
         #endregion 
 
         #region IPfcUnit Members
 
-        public string Name {
-            get {
-                return m_name;
+        public string Name
+        {
+            get
+            {
+                return _name;
             }
-            set {
-                m_name = value;
+            set
+            {
+                _name = value;
             }
         }
 
-        public int SequenceNumber {
-            get {
-                return m_sequenceNumber;
+        public int SequenceNumber
+        {
+            get
+            {
+                return _sequenceNumber;
             }
-            set {
-                m_sequenceNumber = value;
+            set
+            {
+                _sequenceNumber = value;
             }
         }
 
