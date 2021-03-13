1928be522448dd9ff2c752e67e2025c00bfa25b8
stuarthillary <shillary.git@gmail.com>
2021-03-13 22:30:03 +0000

    ExecFactory cleanup

--- Sage/Core/ExecFactory.cs
+++ Sage/Core/ExecFactory.cs
@@ -3,42 +3,29 @@
 #define TIME_BOUNDED
 
 using System;
-using System.Collections.Specialized;
-using System.Configuration;
-using System.Diagnostics;
 using System.Reflection;
 #if LICENSING_ENABLED
 using Highpoint.Sage.Licensing;
 #endif // LICENSING_ENABLED
 
-using _Debug = System.Diagnostics.Debug;
 
-namespace Highpoint.Sage.SimCore {
-
-    public enum ExecType
-    {
-        /// <summary>
-        /// The full featured executive is multi-threaded, and supports the full API.
-        /// </summary>
-        FullFeatured,
-        /// <summary>
-        /// The single threaded executive obtains highest performance through a limited feature set.
-        /// </summary>
-        SingleThreaded
-    }
+namespace Highpoint.Sage.SimCore
+{
 
     /// <summary>
     /// ExecFactory produces instances of objects that implement IExecutive.
     /// </summary>
-    public class ExecFactory {
+    public class ExecFactory
+    {
 
 #if LICENSING_ENABLED
         private static bool _licenseChecked = true;
 #endif // LICENSING_ENABLED
-        private static readonly object s_lock = new object();
+        private static readonly object _lock = new object();
         private static volatile ExecFactory _instance;
 
-        private ExecFactory() {
+        private ExecFactory()
+        {
 #if LICENSING_ENABLED
 #if TIME_BOUNDED
             DateTime expiry = new DateTime(2016, 12, 31);
@@ -75,10 +62,16 @@ namespace Highpoint.Sage.SimCore {
         /// <summary>
         /// Provides a reference to the one ExecFactory in the current Application Context.
         /// </summary>
-        public static ExecFactory Instance {
-            get {
+        public static ExecFactory Instance
+        {
+            get
+            {
 
-                lock (s_lock) { if (_instance == null) _instance = new ExecFactory(); }
+                lock (_lock)
+                {
+                    if (_instance == null)
+                        _instance = new ExecFactory();
+                }
                 return _instance;
             }
         }
@@ -87,7 +80,8 @@ namespace Highpoint.Sage.SimCore {
         /// Creates a copy of the default type of executive.
         /// </summary>
         /// <returns>An instance of the default executive type.</returns>
-        public IExecutive CreateExecutive() {
+        public IExecutive CreateExecutive()
+        {
             return CreateExecutive(Guid.NewGuid());
         }
 
@@ -109,10 +103,10 @@ namespace Highpoint.Sage.SimCore {
         /// <returns>IExecutive.</returns>
         public IExecutive CreateExecutive(ExecType execType, Guid guid)
         {
-            switch(execType)
+            switch (execType)
             {
                 case ExecType.FullFeatured:
-                    return CreateExecutive(typeof (Highpoint.Sage.SimCore.Executive).FullName, guid);
+                    return CreateExecutive(typeof(Highpoint.Sage.SimCore.Executive).FullName, guid);
                 case ExecType.SingleThreaded:
                     return CreateExecutive(typeof(Highpoint.Sage.SimCore.ExecutiveFastLight).FullName, guid);
                 default:
@@ -120,10 +114,12 @@ namespace Highpoint.Sage.SimCore {
             }
         }
 
-        public IExecutive CreateExecutive(string typeName, Guid guid) {
+        public IExecutive CreateExecutive(string typeName, Guid guid)
+        {
 
             Type type = Type.GetType(typeName);
-            if ( type == null ) throw new ApplicationException("Attempt to create an executive of type \"" + typeName + "\", but that type does not exist.");
+            if (type == null)
+                throw new ApplicationException("Attempt to create an executive of type \"" + typeName + "\", but that type does not exist.");
             BindingFlags bindingAttr = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance;
             //Binder binder = null;
             //ParameterModifier[] modifiers = null;
@@ -137,15 +133,17 @@ namespace Highpoint.Sage.SimCore {
         }
 
 
-        private string m_requiredType;
+        private string _requiredType;
         /// <summary>
         /// Creates a copy of the default type of executive.
         /// </summary>
         /// <param name="execGuid">The guid to be assigned to the new executive.</param>
         /// <returns>A copy of the default executive.</returns>
-        public IExecutive CreateExecutive(Guid execGuid) {
-            if (m_requiredType == null) {
-                m_requiredType = "Highpoint.Sage.SimCore.Executive, Sage";
+        public IExecutive CreateExecutive(Guid execGuid)
+        {
+            if (_requiredType == null)
+            {
+                _requiredType = "Highpoint.Sage.SimCore.Executive, Sage";
 #if !NETSTANDARD
                 NameValueCollection nvc = (NameValueCollection)ConfigurationManager.GetSection("Sage");
                 if (nvc == null) {
@@ -155,7 +153,7 @@ namespace Highpoint.Sage.SimCore {
                 }
 #endif
             }
-            return CreateExecutive(m_requiredType, execGuid);
+            return CreateExecutive(_requiredType, execGuid);
         }
     }
 }
---
+++ Sage/Core/ExecType.cs
@@ -0,0 +1,16 @@
+﻿/* This source code licensed under the GNU Affero General Public License */
+
+namespace Highpoint.Sage.SimCore
+{
+    public enum ExecType
+    {
+        /// <summary>
+        /// The full featured executive is multi-threaded, and supports the full API.
+        /// </summary>
+        FullFeatured,
+        /// <summary>
+        /// The single threaded executive obtains highest performance through a limited feature set.
+        /// </summary>
+        SingleThreaded
+    }
+}
