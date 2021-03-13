159b4a0707a0e2378ce767a7b2213e6fe07cb942
stuarthillary <shillary.git@gmail.com>
2021-03-13 01:36:52 +0000

    PfcTransition cleanup

--- Sage/Graphs/PFC/PfcTransition.cs
+++ Sage/Graphs/PFC/PfcTransition.cs
@@ -1,28 +1,34 @@
 /* This source code licensed under the GNU Affero General Public License */
+using Highpoint.Sage.Graphs.PFC.Execution;
 using System;
 using System.Collections.Generic;
-using Highpoint.Sage.Graphs.PFC.Execution;
 
-namespace Highpoint.Sage.Graphs.PFC {
+namespace Highpoint.Sage.Graphs.PFC
+{
 
     /// <summary>
     /// A PfcTransition acts as a Transition in a Pfc (Procedure Function Chart).
     /// </summary>
-    public class PfcTransition : PfcNode, IPfcTransitionNode {
+    public class PfcTransition : PfcNode, IPfcTransitionNode
+    {
 
         #region Private Fields
 
-        private ExecutableCondition m_executableExpression;
-        private Expressions.Expression m_expression = null;
-        private TransitionStateMachine m_myTransitionStateMachine = null;
+        private ExecutableCondition _executableExpression;
+        private Expressions.Expression _expression = null;
+        private TransitionStateMachine _myTransitionStateMachine = null;
 
         #endregion Private Fields
 
         #region Static Fields
         public static ExecutableCondition DefaultExecutableExpression
-            = new ExecutableCondition(delegate(object userData, TransitionStateMachine tsm) {
-            return tsm.PredecessorStateMachines.TrueForAll(delegate(StepStateMachine ssm) { return ssm.GetState((PfcExecutionContext)userData) == StepState.Complete; });
-        });
+            = new ExecutableCondition(delegate (object userData, TransitionStateMachine tsm)
+            {
+                return tsm.PredecessorStateMachines.TrueForAll(delegate (StepStateMachine ssm)
+                {
+                    return ssm.GetState((PfcExecutionContext)userData) == StepState.Complete;
+                });
+            });
 
 
 
@@ -32,8 +38,8 @@ namespace Highpoint.Sage.Graphs.PFC {
         /// </summary>
         public static string DefaultExpression = Expressions.PredecessorsComplete.NAME;
 
-        #endregion 
-        
+        #endregion
+
         #region Constructors
 
         /// <summary>
@@ -49,9 +55,10 @@ namespace Highpoint.Sage.Graphs.PFC {
         /// <param name="description">The description for this transition.</param>
         /// <param name="guid">The GUID of this transition.</param>
         public PfcTransition(IProcedureFunctionChart parent, string name, string description, Guid guid)
-            : base(parent, name, description, guid) {
-            m_expression = Expressions.Expression.FromUf(DefaultExpression,parent.ParticipantDirectory, this);
-            m_executableExpression = DefaultExecutableExpression;
+            : base(parent, name, description, guid)
+        {
+            _expression = Expressions.Expression.FromUf(DefaultExpression, parent.ParticipantDirectory, this);
+            _executableExpression = DefaultExecutableExpression;
         }
 
         #endregion Constructors
@@ -62,18 +69,27 @@ namespace Highpoint.Sage.Graphs.PFC {
         /// Gets the expression that is attached to this transition.
         /// </summary>
         /// <value>The expression.</value>
-        public Expressions.Expression Expression { get { return m_expression; } }
+        public Expressions.Expression Expression
+        {
+            get
+            {
+                return _expression;
+            }
+        }
 
         /// <summary>
         /// Gets or sets the 'friendly' value of this expression. Uses step names and macro names.
         /// </summary>
         /// <value>The expression value.</value>
-        public string ExpressionUFValue {
-            get {
+        public string ExpressionUFValue
+        {
+            get
+            {
                 return Expression.ToString(Expressions.ExpressionType.Friendly, this);
             }
-            set {
-                m_expression = Expressions.Expression.FromUf(value, Parent.ParticipantDirectory, this);
+            set
+            {
+                _expression = Expressions.Expression.FromUf(value, Parent.ParticipantDirectory, this);
             }
         }
 
@@ -81,12 +97,15 @@ namespace Highpoint.Sage.Graphs.PFC {
         /// Gets or sets the 'hostile' value of this expression.
         /// </summary>
         /// <value>The expression value.</value>
-        public string ExpressionUHValue {
-            get {
+        public string ExpressionUHValue
+        {
+            get
+            {
                 return Expression.ToString(Expressions.ExpressionType.Hostile, this);
             }
-            set {
-                m_expression = Expressions.Expression.FromUh(value, Parent.ParticipantDirectory, this);
+            set
+            {
+                _expression = Expressions.Expression.FromUh(value, Parent.ParticipantDirectory, this);
             }
         }
 
@@ -94,8 +113,10 @@ namespace Highpoint.Sage.Graphs.PFC {
         /// Gets the expanded value of this expression. Uses step names and expands macro names into their resultant names.
         /// </summary>
         /// <value>The expanded value of this expression.</value>
-        public string ExpressionExpandedValue {
-            get {
+        public string ExpressionExpandedValue
+        {
+            get
+            {
                 return Expression.ToString(Expressions.ExpressionType.Expanded, this);
             }
         }
@@ -105,26 +126,43 @@ namespace Highpoint.Sage.Graphs.PFC {
         /// evaluate unless overridden in the execution manager.
         /// </summary>
         /// <value>The default executable condition.</value>
-        public ExecutableCondition ExpressionExecutable { get { return m_executableExpression; } set { m_executableExpression = value; } }
-
-        public TransitionStateMachine MyTransitionStateMachine {
-            get {
-                if (m_myTransitionStateMachine == null) {
-                    object obj = ( (ProcedureFunctionChart)Parent ).ExecutionEngine; // Forces initialization so everyone has a TSM.
-                }
-                return m_myTransitionStateMachine;
+        public ExecutableCondition ExpressionExecutable
+        {
+            get
+            {
+                return _executableExpression;
             }
-            internal set {
-                if (m_myTransitionStateMachine != null && value != null) {
-                    string message = string.Format(s_msg_Replace_Existing_Sm, Name);
+            set
+            {
+                _executableExpression = value;
+            }
+        }
+
+        public TransitionStateMachine MyTransitionStateMachine
+        {
+            get
+            {
+                if (_myTransitionStateMachine == null)
+                {
+                    object obj = ((ProcedureFunctionChart)Parent).ExecutionEngine; // Forces initialization so everyone has a TSM.
+                }
+                return _myTransitionStateMachine;
+            }
+            internal set
+            {
+                if (_myTransitionStateMachine != null && value != null)
+                {
+                    string message = string.Format(_msgReplaceExistingSm, Name);
                     throw new ApplicationException(message);
-                } else {
-                    m_myTransitionStateMachine = value;
+                }
+                else
+                {
+                    _myTransitionStateMachine = value;
                 }
             }
         }
 
-        private static readonly string s_msg_Replace_Existing_Sm = "Attempt to replace an existing step state machine on {0}. " 
+        private static readonly string _msgReplaceExistingSm = "Attempt to replace an existing step state machine on {0}. "
             + "This could be due to initializing the Execution Engine on a PFC before that PFC's construction is complete.";
 
         #endregion
@@ -135,8 +173,12 @@ namespace Highpoint.Sage.Graphs.PFC {
         /// Gets the type of this element.
         /// </summary>
         /// <value>The type of the element.</value>
-        public override PfcElementType ElementType {
-            get { return PfcElementType.Transition; }
+        public override PfcElementType ElementType
+        {
+            get
+            {
+                return PfcElementType.Transition;
+            }
         }
 
         /// <summary>
@@ -144,25 +186,33 @@ namespace Highpoint.Sage.Graphs.PFC {
         /// eliminated when PFCs are combined.
         /// </summary>
         /// <value><c>true</c> if this instance is null; otherwise, <c>false</c>.</value>
-        public override bool IsNullNode {
-            get {
+        public override bool IsNullNode
+        {
+            get
+            {
                 return (/*PredecessorNodes.Count < 2 && SuccessorNodes.Count < 2 &&*/ ExpressionUFValue.Equals(DefaultExpression));
             }
-            set {
+            set
+            {
                 //System.Diagnostics.Debug.Assert(value == true, "Transitions are non-null only by virtue of their expressions.");
             }
         }
 
         #endregion 
 
-        public static IPfcTransitionNode Between(IPfcStepNode before, IPfcStepNode after) {
-            return (IPfcTransitionNode)before.SuccessorNodes.Find(delegate(IPfcNode trans) { return trans.SuccessorNodes.Contains(after); });
+        public static IPfcTransitionNode Between(IPfcStepNode before, IPfcStepNode after)
+        {
+            return (IPfcTransitionNode)before.SuccessorNodes.Find(delegate (IPfcNode trans)
+            {
+                return trans.SuccessorNodes.Contains(after);
+            });
         }
 
         /// <summary>
         /// The TransitionComparer is used to sort transitions by their graph ordinals.
         /// </summary>
-        public class TransitionComparer : IComparer<IPfcTransitionNode> {
+        public class TransitionComparer : IComparer<IPfcTransitionNode>
+        {
             /// <summary>
             /// Compares two IPfcTransitionNodes and returns a value indicating whether one is less than, equal to, or greater than the other.
             /// </summary>
@@ -171,9 +221,11 @@ namespace Highpoint.Sage.Graphs.PFC {
             /// <returns>
             /// Value Condition Less than zero, x is less than y. Zero, x equals y.Greater than zero, x is greater than y.
             /// </returns>
-            public int Compare(IPfcTransitionNode x, IPfcTransitionNode y) {
+            public int Compare(IPfcTransitionNode x, IPfcTransitionNode y)
+            {
                 int retval = Comparer<int>.Default.Compare(x.GraphOrdinal, y.GraphOrdinal);
-                if (retval == 0) {
+                if (retval == 0)
+                {
                     retval = Utility.GuidOps.Compare(x.Guid, y.Guid);
                 }
                 return retval;
