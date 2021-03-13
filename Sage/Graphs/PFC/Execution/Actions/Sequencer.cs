01308cb5f77530908415a60c6d0c72df3cffc481
stuarthillary <shillary.git@gmail.com>
2021-03-13 22:10:01 +0000

    Sequencer cleanup

--- Sage/Graphs/PFC/Execution/Actions/Sequencer.cs
+++ Sage/Graphs/PFC/Execution/Actions/Sequencer.cs
@@ -1,9 +1,10 @@
 /* This source code licensed under the GNU Affero General Public License */
-using System;
 using Highpoint.Sage.SimCore;
 using Highpoint.Sage.Utility;
+using System;
 
-namespace Highpoint.Sage.Graphs.PFC.Execution.Actions {
+namespace Highpoint.Sage.Graphs.PFC.Execution.Actions
+{
 
     /// <summary>
     /// When a sequencer's Precondition is set as the precondition of a pfcStep, it will watch the PFCExecutionContext
@@ -13,12 +14,13 @@ namespace Highpoint.Sage.Graphs.PFC.Execution.Actions {
     /// exchange on which the successor is already waiting. Only when the successor receives that key will it grant
     /// itself permission to run.
     /// </summary>
-    public class Sequencer {
+    public class Sequencer
+    {
 
-        private Guid m_sequenceKey;
-        private Guid m_myKey;
-        private int m_myIndex;
-        private int m_rootHeight;
+        private Guid _sequenceKey;
+        private Guid _myKey;
+        private readonly int _myIndex;
+        private readonly int _rootHeight;
         private IDetachableEventController m_idec = null;
 
         /// <summary>
@@ -27,60 +29,74 @@ namespace Highpoint.Sage.Graphs.PFC.Execution.Actions {
         /// <param name="sequencerKey">The sequencer key.</param>
         /// <param name="myIndex">The creator's index.</param>
         /// <param name="rootHeight">Height of the root.</param>
-        public Sequencer(Guid sequencerKey, int myIndex, int rootHeight){
-            m_sequenceKey = sequencerKey;
-            m_myIndex = myIndex;
-            m_myKey = GuidOps.Add(sequencerKey, myIndex);
-            m_rootHeight = rootHeight;
+        public Sequencer(Guid sequencerKey, int myIndex, int rootHeight)
+        {
+            _sequenceKey = sequencerKey;
+            _myIndex = myIndex;
+            _myKey = GuidOps.Add(sequencerKey, myIndex);
+            _rootHeight = rootHeight;
         }
 
         public Sequencer(Guid sequencerKey, int myIndex)
-            : this(sequencerKey, myIndex, 0) {}
+            : this(sequencerKey, myIndex, 0) { }
 
-        public PfcAction Precondition {
-            get {
+        public PfcAction Precondition
+        {
+            get
+            {
                 return new PfcAction(GetPermissionToStart);
             }
         }
 
-        protected void GetPermissionToStart(PfcExecutionContext myPfcec, StepStateMachine ssm) {
+        protected void GetPermissionToStart(PfcExecutionContext myPfcec, StepStateMachine ssm)
+        {
 
             PfcExecutionContext root = myPfcec;
-            int ascents = m_rootHeight;
-            while (ascents > 0) {
+            int ascents = _rootHeight;
+            while (ascents > 0)
+            {
                 root = (PfcExecutionContext)myPfcec.Parent.Payload;
                 ascents--;
             }
-            
+
             Exchange exchange = null;
-            if (m_myIndex == 0) {
+            if (_myIndex == 0)
+            {
                 //Console.WriteLine(myPfcec.Name + " is creating an exchange and injecting it into pfcec " + root.Name + " under key " + m_sequenceKey);
                 exchange = new Exchange(myPfcec.Model.Executive);
-                root.Add(m_sequenceKey, exchange);
-            } else {
+                root.Add(_sequenceKey, exchange);
+            }
+            else
+            {
                 //Console.WriteLine(myPfcec.Name + " is looking for an exchange in pfcec " + root.Name + " under key " + m_sequenceKey);
                 DictionaryChange dc = new DictionaryChange(myPfcec_EntryAdded);
-                while (true) {
-                    exchange = (Exchange)root[m_sequenceKey];
-                    if (exchange == null) {
+                while (true)
+                {
+                    exchange = (Exchange)root[_sequenceKey];
+                    if (exchange == null)
+                    {
                         root.EntryAdded += dc;
                         m_idec = myPfcec.Model.Executive.CurrentEventController;
                         m_idec.Suspend();
-                    } else {
+                    }
+                    else
+                    {
                         root.EntryAdded -= dc;
                         break;
                     }
                 }
-                exchange.Take(m_myKey, true); // Only indices 1,2, ... take (and wait?). Index 0 only posts.
+                exchange.Take(_myKey, true); // Only indices 1,2, ... take (and wait?). Index 0 only posts.
                 //Console.WriteLine(myPfcec.Name + " got the key I was looking for!");
             }
-            Guid nextGuysKey = GuidOps.Increment(m_myKey);
+            Guid nextGuysKey = GuidOps.Increment(_myKey);
             exchange.Post(nextGuysKey, nextGuysKey, false);
             //Console.WriteLine(myPfcec.Name + " posted the key the next guy is looking for!");
         }
 
-        void myPfcec_EntryAdded(object key, object value) {
-            if (key.Equals(m_sequenceKey)) {
+        void myPfcec_EntryAdded(object key, object value)
+        {
+            if (key.Equals(_sequenceKey))
+            {
                 m_idec.Resume();
             }
         }
