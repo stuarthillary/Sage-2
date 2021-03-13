a428d0a81763d0243a731bc5b3477744b466bc1b
stuarthillary <shillary.git@gmail.com>
2021-03-13 21:55:08 +0000

    PfcActor cleanup

--- Sage/Graphs/PFC/Execution/PfcActor.cs
+++ Sage/Graphs/PFC/Execution/PfcActor.cs
@@ -1,22 +1,28 @@
 /* This source code licensed under the GNU Affero General Public License */
+using Highpoint.Sage.SimCore;
 using System;
 using System.Collections.Generic;
-using Highpoint.Sage.SimCore;
 
-namespace Highpoint.Sage.Graphs.PFC.Execution {
+namespace Highpoint.Sage.Graphs.PFC.Execution
+{
 
-    public abstract class PfcActor : IModelObject {
+    public abstract class PfcActor : IModelObject
+    {
 
         #region Private Fields
-        private readonly IPfcStepNode m_myStepNode;
+        private readonly IPfcStepNode _myStepNode;
         #endregion Private Fields
 
         #region Constructors
 
-        protected PfcActor(IModel model, string name, Guid guid, IPfcStepNode myStepNode) {
-            model.Starting += delegate { m_peers = null; };
+        protected PfcActor(IModel model, string name, Guid guid, IPfcStepNode myStepNode)
+        {
+            model.Starting += delegate
+            {
+                _peers = null;
+            };
             InitializeIdentity(model, name, null, guid);
-            m_myStepNode = myStepNode;
+            _myStepNode = myStepNode;
         }
 
         #endregion Constructors
@@ -25,18 +31,22 @@ namespace Highpoint.Sage.Graphs.PFC.Execution {
 
         public abstract void Run(PfcExecutionContext parameters, StepStateMachine ssm);
 
-        public IPfcStepNode StepNode => m_myStepNode;
+        public IPfcStepNode StepNode => _myStepNode;
 
-        private List<IPfcStepNode> m_peers;
-        public List<IPfcStepNode> Peers {
-            get {
-                if (m_peers == null) {
-                    m_peers = new List<IPfcStepNode>();
-                    foreach (IPfcStepNode step in m_myStepNode.Parent.Steps) {
-                        m_peers.Add(step);
+        private List<IPfcStepNode> _peers;
+        public List<IPfcStepNode> Peers
+        {
+            get
+            {
+                if (_peers == null)
+                {
+                    _peers = new List<IPfcStepNode>();
+                    foreach (IPfcStepNode step in _myStepNode.Parent.Steps)
+                    {
+                        _peers.Add(step);
                     }
                 }
-                return m_peers;
+                return _peers;
             }
         }
 
@@ -45,36 +55,64 @@ namespace Highpoint.Sage.Graphs.PFC.Execution {
         /// </summary>
         /// <param name="mode">The stochastic mode.</param>
         public abstract void SetStochasticMode(StochasticMode mode);
-    
+
         #region Implementation of IModelObject
-        private string m_name;
-        private Guid m_guid = Guid.Empty;
-        private IModel m_model;
-		private string m_description;
-        
+        private string _name;
+        private Guid _guid = Guid.Empty;
+        private IModel _model;
+        private string _description;
+
         /// <summary>
         /// The IModel to which this object belongs.
         /// </summary>
         /// <value>The object's Model.</value>
-        public IModel Model { [System.Diagnostics.DebuggerStepThrough] get { return m_model; } }
-       
+        public IModel Model
+        {
+            [System.Diagnostics.DebuggerStepThrough]
+            get
+            {
+                return _model;
+            }
+        }
+
         /// <summary>
         /// The name by which this object is known. Typically not required to be unique in a pan-model context.
         /// </summary>
         /// <value>The object's name.</value>
-        public string Name { [System.Diagnostics.DebuggerStepThrough]get { return m_name; } }
-        
+        public string Name
+        {
+            [System.Diagnostics.DebuggerStepThrough]
+            get
+            {
+                return _name;
+            }
+        }
+
         /// <summary>
         /// The description for this object. Typically used for human-readable representations.
         /// </summary>
         /// <value>The object's description.</value>
-		public string Description { [System.Diagnostics.DebuggerStepThrough] get { return (m_description ?? ("No description for " + m_name)); } }
-        
+        public string Description
+        {
+            [System.Diagnostics.DebuggerStepThrough]
+            get
+            {
+                return (_description ?? ("No description for " + _name));
+            }
+        }
+
         /// <summary>
         /// The Guid for this object. Typically required to be unique in a pan-model context.
         /// </summary>
         /// <value>The object's Guid.</value>
-        public Guid Guid { [System.Diagnostics.DebuggerStepThrough] get { return m_guid; } }
+        public Guid Guid
+        {
+            [System.Diagnostics.DebuggerStepThrough]
+            get
+            {
+                return _guid;
+            }
+        }
 
         /// <summary>
         /// Initializes the fields that feed the properties of this IModelObject identity.
@@ -83,12 +121,16 @@ namespace Highpoint.Sage.Graphs.PFC.Execution {
         /// <param name="name">The IModelObject's new name value.</param>
         /// <param name="description">The IModelObject's new description value.</param>
         /// <param name="guid">The IModelObject's new GUID value.</param>
-        public void InitializeIdentity(IModel model, string name, string description, Guid guid) {
-            IMOHelper.Initialize(ref m_model, model, ref m_name, name, ref m_description, description, ref m_guid, guid);
+        public void InitializeIdentity(IModel model, string name, string description, Guid guid)
+        {
+            IMOHelper.Initialize(ref _model, model, ref _name, name, ref _description, description, ref _guid, guid);
         }
         #endregion
 
-        public enum StochasticMode { Full, Schedule }
+        public enum StochasticMode
+        {
+            Full, Schedule
+        }
 
     }
 }
