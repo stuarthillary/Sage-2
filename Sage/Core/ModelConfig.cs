a0238c2137a01f422bf38c03958eefae2def7568
stuarthillary <shillary.git@gmail.com>
2021-03-14 00:21:55 +0000

    ModelConfig cleanup

--- Sage/Core/ModelConfig.cs
+++ Sage/Core/ModelConfig.cs
@@ -1,36 +1,43 @@
 /* This source code licensed under the GNU Affero General Public License */
 
+using System.Collections.Specialized;
 using System.Diagnostics;
 using _Debug = System.Diagnostics.Debug;
-using System.Collections.Specialized;
 
 
-namespace Highpoint.Sage.SimCore {
+namespace Highpoint.Sage.SimCore
+{
 
     /// <summary>
     /// Class ModelConfig is a collection of initialization parameters that is intended to be available to the model in the app.config file.
     /// </summary>
-    public class ModelConfig {
-		NameValueCollection m_nvc;
+    public class ModelConfig
+    {
+        readonly NameValueCollection _nvc;
         public ModelConfig() : this("Sage") { }
 
-        public ModelConfig(string sectionName) {
-            m_nvc = (NameValueCollection)System.Configuration.ConfigurationManager.GetSection(sectionName);
-            if (m_nvc == null) {
+        public ModelConfig(string sectionName)
+        {
+            _nvc = (NameValueCollection)System.Configuration.ConfigurationManager.GetSection(sectionName);
+            if (_nvc == null)
+            {
                 // TODO: Add this to an Errors & Warnings collection instead of dumping it to Trace.
                 _Debug.WriteLine(string.Format("Warning - <{0}> section missing from config file for {1}.", sectionName, Process.GetCurrentProcess().ProcessName));
             }
         }
 
-        public string GetSimpleParameter(string key) {
-			string retval = null;
-			if ( m_nvc != null ) retval = m_nvc[key];
-			if ( retval == null ) {
-				// TODO: Add this to an Errors & Warnings collection instead of dumping it to Trace.
-				_Debug.WriteLine("Application requested unfound parameter associated with key " + key + " in the app.config file.");
-			}
-			return retval;
-		}
-	}
+        public string GetSimpleParameter(string key)
+        {
+            string retval = null;
+            if (_nvc != null)
+                retval = _nvc[key];
+            if (retval == null)
+            {
+                // TODO: Add this to an Errors & Warnings collection instead of dumping it to Trace.
+                _Debug.WriteLine("Application requested unfound parameter associated with key " + key + " in the app.config file.");
+            }
+            return retval;
+        }
+    }
 
 }
