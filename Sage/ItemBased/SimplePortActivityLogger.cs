a6727f048773c741300a773dbb0d80af7e5319a1
stuarthillary <shillary.git@gmail.com>
2021-03-14 22:56:24 +0000

    SimplePortActivityLogger cleanup

--- Sage/ItemBased/SimplePortActivityLogger.cs
+++ Sage/ItemBased/SimplePortActivityLogger.cs
@@ -1,43 +1,55 @@
 /* This source code licensed under the GNU Affero General Public License */
 
-using _Debug = System.Diagnostics.Debug;
-using System.Text;
-using System.Collections;
 using Highpoint.Sage.ItemBased.Ports;
+using System.Collections;
+using System.Text;
 
-namespace Highpoint.Sage.ItemBased {
-
-    class SimplePortActivityLogger {
-        private StringBuilder m_contents = new StringBuilder();
-        private ArrayList m_arrayList = new ArrayList();
-        private IPort m_port;
-        public SimplePortActivityLogger(IPort port){
-            m_port = port;
-            port.PortDataPresented+=new PortDataEvent(PortDataPresented);
-            port.PortDataAccepted+=new PortDataEvent(PortDataAccepted);
-            port.PortDataRejected+=new PortDataEvent(PortDataRejected);
+namespace Highpoint.Sage.ItemBased
+{
+    internal class SimplePortActivityLogger
+    {
+        private readonly StringBuilder _contents = new StringBuilder();
+        private readonly ArrayList _arrayList = new ArrayList();
+        private readonly IPort _port;
+        public SimplePortActivityLogger(IPort port)
+        {
+            _port = port;
+            port.PortDataPresented += new PortDataEvent(PortDataPresented);
+            port.PortDataAccepted += new PortDataEvent(PortDataAccepted);
+            port.PortDataRejected += new PortDataEvent(PortDataRejected);
         }
 
-        public SimplePortActivityLogger(IPortSet portSet){
-            foreach ( IPort port in portSet ) {
-                port.PortDataPresented+=new PortDataEvent(PortDataPresented);
-                port.PortDataAccepted+=new PortDataEvent(PortDataAccepted);
-                port.PortDataRejected+=new PortDataEvent(PortDataRejected);
+        public SimplePortActivityLogger(IPortSet portSet)
+        {
+            foreach (IPort port in portSet)
+            {
+                port.PortDataPresented += new PortDataEvent(PortDataPresented);
+                port.PortDataAccepted += new PortDataEvent(PortDataAccepted);
+                port.PortDataRejected += new PortDataEvent(PortDataRejected);
             }
         }
 
-        void PortDataPresented(object data, IPort port){
-            m_contents.Append("A port on " + port.Owner + " was presented with " + data + "\r\n");
+        void PortDataPresented(object data, IPort port)
+        {
+            _contents.Append("A port on " + port.Owner + " was presented with " + data + "\r\n");
         }
 
-        void PortDataAccepted(object data, IPort port){
-            m_contents.Append("A port on " + port.Owner + " accepted " + data + "\r\n");
+        void PortDataAccepted(object data, IPort port)
+        {
+            _contents.Append("A port on " + port.Owner + " accepted " + data + "\r\n");
         }
 
-        void PortDataRejected(object data, IPort port){
-            m_contents.Append("A port on " + port.Owner + " rejected " + data + "\r\n");
+        void PortDataRejected(object data, IPort port)
+        {
+            _contents.Append("A port on " + port.Owner + " rejected " + data + "\r\n");
         }
 
-        public string Contents { get { return m_contents.ToString(); } }
+        public string Contents
+        {
+            get
+            {
+                return _contents.ToString();
+            }
+        }
     }
 }
