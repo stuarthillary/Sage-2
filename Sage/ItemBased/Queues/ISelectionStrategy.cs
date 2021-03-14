d45b66d000bdcd458b00613a1caeb4b530a377a9
stuarthillary <shillary.git@gmail.com>
2021-03-14 01:08:13 +0000

    IQueue cleanup

---
+++ Sage/ItemBased/Queues/ISelectionStrategy.cs
@@ -0,0 +1,15 @@
+﻿/* This source code licensed under the GNU Affero General Public License */
+
+using System.Collections;
+
+namespace Highpoint.Sage.ItemBased.Queues
+{
+    public interface ISelectionStrategy
+    {
+        object GetNext(object context);
+        ICollection Candidates
+        {
+            get; set;
+        }
+    }
+}
