6191ef2cfad9210f0a1e979a4e7da588dc473157
stuarthillary <shillary.git@gmail.com>
2021-03-14 23:11:07 +0000

    ReactionInstance cleanup

--- Sage/Materials/Chemistry/ReactionInstance.cs
+++ Sage/Materials/Chemistry/ReactionInstance.cs
@@ -2,7 +2,8 @@
 
 using System;
 
-namespace Highpoint.Sage.Materials.Chemistry {
+namespace Highpoint.Sage.Materials.Chemistry
+{
     /// <summary>
     /// A ReactionInstance is created every time a reaction takes place. It records
     /// what reaction took place, how much of the reaction took place (i.e. percent
@@ -11,65 +12,81 @@ namespace Highpoint.Sage.Materials.Chemistry {
     /// value were to be changed in the reaction, later (such as as a result of a
     /// change in temperature.)
     /// </summary>
-    public class ReactionInstance {
+    public class ReactionInstance
+    {
 
-        private readonly Reaction m_reaction;
-        private readonly double m_fwdScale;
-        private readonly double m_revScale;
+        private readonly Reaction _reaction;
+        private readonly double _fwdScale;
+        private readonly double _revScale;
 
-        public ReactionInstance(Reaction reaction, double fwdScale, double revScale, Guid rxnInstanceGuid) {
-			m_reaction = reaction;
-			m_fwdScale = fwdScale;
-			m_revScale = revScale;
-			Guid     = rxnInstanceGuid;
-		}
-
-//		public ReactionInstance(Reaction reaction, double fwdScale, double revScale)
-//			:this(reaction,fwdScale,revScale,Guid.NewGuid()){}
-
-		public Reaction Reaction => m_reaction;
-
-        private Reaction m_isReaction;
-		public Reaction InstanceSpecificReaction {
-			get { 
-				if ( m_isReaction == null ) {
-					double scale = (m_revScale-m_fwdScale);
-					m_isReaction = new Reaction(m_reaction.Model,"Instance Specific Reaction",Guid.NewGuid());
-					foreach ( Reaction.ReactionParticipant rp in m_reaction.Reactants ) {
-						m_isReaction.AddReactant(rp.MaterialType,rp.Mass*(scale));
-					}
-					foreach ( Reaction.ReactionParticipant rp in m_reaction.Products ) {
-						m_isReaction.AddProduct(rp.MaterialType,rp.Mass*(-scale));
-					}
-				}
-				return m_isReaction;
-			}
-		}
-
-        public double ForwardScale => m_fwdScale;
-
-        public double ReverseScale => m_revScale;
-
-        public double PercentCompletion => m_reaction.PercentCompletion;
-
-        public override string ToString(){
-            return "Reaction " + m_reaction.Name + " occurred with a forward scale of " + m_fwdScale + " and a reverse scale of " + m_revScale + ". Reaction ran to " + (m_reaction.PercentCompletion*100d) + "% completion.";
+        public ReactionInstance(Reaction reaction, double fwdScale, double revScale, Guid rxnInstanceGuid)
+        {
+            _reaction = reaction;
+            _fwdScale = fwdScale;
+            _revScale = revScale;
+            Guid = rxnInstanceGuid;
         }
 
-        public string InstanceSpecificReactionString(){
+        //		public ReactionInstance(Reaction reaction, double fwdScale, double revScale)
+        //			:this(reaction,fwdScale,revScale,Guid.NewGuid()){}
+
+        public Reaction Reaction => _reaction;
+
+        private Reaction _isReaction;
+        public Reaction InstanceSpecificReaction
+        {
+            get
+            {
+                if (_isReaction == null)
+                {
+                    double scale = (_revScale - _fwdScale);
+                    _isReaction = new Reaction(_reaction.Model, "Instance Specific Reaction", Guid.NewGuid());
+                    foreach (Reaction.ReactionParticipant rp in _reaction.Reactants)
+                    {
+                        _isReaction.AddReactant(rp.MaterialType, rp.Mass * (scale));
+                    }
+                    foreach (Reaction.ReactionParticipant rp in _reaction.Products)
+                    {
+                        _isReaction.AddProduct(rp.MaterialType, rp.Mass * (-scale));
+                    }
+                }
+                return _isReaction;
+            }
+        }
+
+        public double ForwardScale => _fwdScale;
+
+        public double ReverseScale => _revScale;
+
+        public double PercentCompletion => _reaction.PercentCompletion;
+
+        public override string ToString()
+        {
+            return "Reaction " + _reaction.Name + " occurred with a forward scale of " + _fwdScale + " and a reverse scale of " + _revScale + ". Reaction ran to " + (_reaction.PercentCompletion * 100d) + "% completion.";
+        }
+
+        public string InstanceSpecificReactionString()
+        {
             System.Text.StringBuilder sb = new System.Text.StringBuilder();
-            for ( int i = 0 ; i < m_reaction.Reactants.Count ; i++ ) {
-                sb.Append(((Reaction.ReactionParticipant)m_reaction.Reactants[i]).ToString(m_fwdScale-m_revScale));
-                if ( i < m_reaction.Reactants.Count-1 ) sb.Append(" + ");
+            for (int i = 0; i < _reaction.Reactants.Count; i++)
+            {
+                sb.Append(((Reaction.ReactionParticipant)_reaction.Reactants[i]).ToString(_fwdScale - _revScale));
+                if (i < _reaction.Reactants.Count - 1)
+                    sb.Append(" + ");
             }
             sb.Append(" <==> ");
-            for ( int i = 0 ; i < m_reaction.Products.Count ; i++ ) {
-                sb.Append(((Reaction.ReactionParticipant)m_reaction.Products[i]).ToString(m_fwdScale-m_revScale));
-                if ( i < m_reaction.Products.Count-1 ) sb.Append(" + ");
+            for (int i = 0; i < _reaction.Products.Count; i++)
+            {
+                sb.Append(((Reaction.ReactionParticipant)_reaction.Products[i]).ToString(_fwdScale - _revScale));
+                if (i < _reaction.Products.Count - 1)
+                    sb.Append(" + ");
             }
             return sb.ToString();
         }
 
-        public Guid Guid { get; }
+        public Guid Guid
+        {
+            get;
+        }
     }
 }
