ab410e6710e611aaee638510acf6bbf1c3a908c2
stuarthillary <shillary.git@gmail.com>
2021-03-14 23:59:29 +0000

    MaterialConduitManager cleanup

--- Sage/Materials/MaterialConduitManager.cs
+++ Sage/Materials/MaterialConduitManager.cs
@@ -1,9 +1,10 @@
 /* This source code licensed under the GNU Affero General Public License */
 
-using System.Collections;
 using Highpoint.Sage.Resources;
+using System.Collections;
 
-namespace Highpoint.Sage.Materials.Chemistry {
+namespace Highpoint.Sage.Materials.Chemistry
+{
 
     /// <summary>
     /// A MaterialConduitManager handles replenishment and drawdown from a primary 
@@ -13,12 +14,13 @@ namespace Highpoint.Sage.Materials.Chemistry {
     /// intermediary between a ready tank and an inventory tank, or a waste tank and
     /// a reclamation plant.
     /// </summary>
-    public class MaterialConduitManager {
+    public class MaterialConduitManager
+    {
 
         #region Private Fields
-        private readonly IResourceManager m_myResourceManager;
-        private readonly Hashtable m_conduits;
-        private Hashtable m_resources;
+        private readonly IResourceManager _myResourceManager;
+        private readonly Hashtable _conduits;
+        private Hashtable _resources;
         #endregion
 
 
@@ -26,79 +28,101 @@ namespace Highpoint.Sage.Materials.Chemistry {
         /// Initializes a new instance of the <see cref="MaterialConduitManager"/> class.
         /// </summary>
         /// <param name="mustHaveMaterialsInIt">The must have materials in it.</param>
-        public MaterialConduitManager(IResourceManager mustHaveMaterialsInIt){
-            m_conduits = new Hashtable();
-			m_myResourceManager = mustHaveMaterialsInIt;
-			m_myResourceManager.ResourceRequested +=m_myResourceManager_ResourceRequested;
-			m_myResourceManager.ResourceAdded +=IndexResources;
-			m_myResourceManager.ResourceRemoved +=IndexResources;
-			IndexResources(m_myResourceManager,null);
-		}
+        public MaterialConduitManager(IResourceManager mustHaveMaterialsInIt)
+        {
+            _conduits = new Hashtable();
+            _myResourceManager = mustHaveMaterialsInIt;
+            _myResourceManager.ResourceRequested += myResourceManager_ResourceRequested;
+            _myResourceManager.ResourceAdded += IndexResources;
+            _myResourceManager.ResourceRemoved += IndexResources;
+            IndexResources(_myResourceManager, null);
+        }
 
-		/// <summary>
-		/// Adds a conduit that allows this resource manager to draw from, and push to, another resource manager
-		/// that handles a specified material. The other resource manager is seen as providing a source for the
-		/// material if this resource manager cannot fulfill a request, and provides a destination to absorb any
-		/// more material than can be absorbed by a material resource item in this resource manager.
-		/// </summary>
-		/// <param name="secondary">The resource manager that provides over/underflow support.</param>
-		/// <param name="mt">The material type for which this conduit applies.</param>
-		public void AddConduit(IResourceManager secondary, MaterialType mt){
-			m_conduits.Add(mt,secondary);
-		}
+        /// <summary>
+        /// Adds a conduit that allows this resource manager to draw from, and push to, another resource manager
+        /// that handles a specified material. The other resource manager is seen as providing a source for the
+        /// material if this resource manager cannot fulfill a request, and provides a destination to absorb any
+        /// more material than can be absorbed by a material resource item in this resource manager.
+        /// </summary>
+        /// <param name="secondary">The resource manager that provides over/underflow support.</param>
+        /// <param name="mt">The material type for which this conduit applies.</param>
+        public void AddConduit(IResourceManager secondary, MaterialType mt)
+        {
+            _conduits.Add(mt, secondary);
+        }
 
-		private void m_myResourceManager_ResourceRequested(IResourceRequest irr, IResource resource)
-		{
-		    MaterialResourceRequest resourceRequest = irr as MaterialResourceRequest;
-		    if ( resourceRequest != null ) {
-				MaterialResourceRequest mrr = resourceRequest;
-				//_Debug.WriteLine("I am taking care of a request for " + mrr.QuantityDesired + " kg of " + mrr.MaterialType.Name);
-				IResourceManager rm = (IResourceManager)m_conduits[mrr.MaterialType];
-				if ( rm != null ) {
-					//_Debug.WriteLine("There is a conduit specified for " + mrr.MaterialType.Name + ", and it is " + rm);
-					HandleRequest(mrr,rm);
-				} else {
-					//_Debug.WriteLine("There is no conduit specified for " + mrr.MaterialType.Name);
-				}
-			} else {
-				//_Debug.WriteLine("I am skipping handling of a request for " + irr.QuantityDesired + " units of something.");
-			}
-		}
+        private void myResourceManager_ResourceRequested(IResourceRequest irr, IResource resource)
+        {
+            MaterialResourceRequest resourceRequest = irr as MaterialResourceRequest;
+            if (resourceRequest != null)
+            {
+                MaterialResourceRequest mrr = resourceRequest;
+                //_Debug.WriteLine("I am taking care of a request for " + mrr.QuantityDesired + " kg of " + mrr.MaterialType.Name);
+                IResourceManager rm = (IResourceManager)_conduits[mrr.MaterialType];
+                if (rm != null)
+                {
+                    //_Debug.WriteLine("There is a conduit specified for " + mrr.MaterialType.Name + ", and it is " + rm);
+                    HandleRequest(mrr, rm);
+                }
+                else
+                {
+                    //_Debug.WriteLine("There is no conduit specified for " + mrr.MaterialType.Name);
+                }
+            }
+            else
+            {
+                //_Debug.WriteLine("I am skipping handling of a request for " + irr.QuantityDesired + " units of something.");
+            }
+        }
 
-	    private void HandleRequest(MaterialResourceRequest mrr, IResourceManager secondary){
-			MaterialResourceItem mri = (MaterialResourceItem)m_resources[mrr.MaterialType];
-			if ( mri == null ) return;
+        private void HandleRequest(MaterialResourceRequest mrr, IResourceManager secondary)
+        {
+            MaterialResourceItem mri = (MaterialResourceItem)_resources[mrr.MaterialType];
+            if (mri == null)
+                return;
 
-			if ( mrr.QuantityDesired > 0 ) { // Depletion - do we have enough supply?
-				if ( mrr.QuantityDesired > (mri.Available+mri.PermissibleOverbook) ) TransferIn(mrr,secondary,(mrr.QuantityDesired-mri.Available));
-			} else { // Augmentation - do we have room?
-				double howMuchRoom = (mri.Capacity+mri.PermissibleOverbook) - mri.Available;
-				if ( howMuchRoom + mrr.QuantityDesired < 0 ) TransferOut(mrr,secondary,(-mrr.QuantityDesired-howMuchRoom));
-			}	 
-		}
+            if (mrr.QuantityDesired > 0)
+            { // Depletion - do we have enough supply?
+                if (mrr.QuantityDesired > (mri.Available + mri.PermissibleOverbook))
+                    TransferIn(mrr, secondary, (mrr.QuantityDesired - mri.Available));
+            }
+            else
+            { // Augmentation - do we have room?
+                double howMuchRoom = (mri.Capacity + mri.PermissibleOverbook) - mri.Available;
+                if (howMuchRoom + mrr.QuantityDesired < 0)
+                    TransferOut(mrr, secondary, (-mrr.QuantityDesired - howMuchRoom));
+            }
+        }
 
-		private void TransferIn(MaterialResourceRequest mrr, IResourceManager secondary, double quantity){
-			//_Debug.WriteLine("Gotta transfer " + quantity + " kg in.");
-			IResourceRequest newMrr = new MaterialResourceRequest(mrr.MaterialType, quantity, MaterialResourceRequest.Direction.Deplete);
-			if ( secondary.Acquire(newMrr,false) ) {
-				//_Debug.WriteLine("Successfully transferred.");
-			}
-		}
+        private void TransferIn(MaterialResourceRequest mrr, IResourceManager secondary, double quantity)
+        {
+            //_Debug.WriteLine("Gotta transfer " + quantity + " kg in.");
+            IResourceRequest newMrr = new MaterialResourceRequest(mrr.MaterialType, quantity, MaterialResourceRequest.Direction.Deplete);
+            if (secondary.Acquire(newMrr, false))
+            {
+                //_Debug.WriteLine("Successfully transferred.");
+            }
+        }
 
-		private void TransferOut(MaterialResourceRequest mrr, IResourceManager secondary, double quantity){
-			//_Debug.WriteLine("Gotta transfer " + quantity + " kg out.");
-			IResourceRequest newMrr = new MaterialResourceRequest(mrr.MaterialType, quantity, MaterialResourceRequest.Direction.Deplete);
-			if ( secondary.Acquire(newMrr,false) ) {
-				//_Debug.WriteLine("Successfully transferred.");
-			}
-		}
+        private void TransferOut(MaterialResourceRequest mrr, IResourceManager secondary, double quantity)
+        {
+            //_Debug.WriteLine("Gotta transfer " + quantity + " kg out.");
+            IResourceRequest newMrr = new MaterialResourceRequest(mrr.MaterialType, quantity, MaterialResourceRequest.Direction.Deplete);
+            if (secondary.Acquire(newMrr, false))
+            {
+                //_Debug.WriteLine("Successfully transferred.");
+            }
+        }
 
-		private void IndexResources(IResourceManager irm, IResource resource) {
-			m_resources = new Hashtable();
-			foreach ( IResource rsc in m_myResourceManager.Resources ) {
-				if ( !( rsc is MaterialResourceItem ) ) continue; 
-				m_resources.Add(((MaterialResourceItem)rsc).MaterialType,rsc);
-			}
-		}
-	}
+        private void IndexResources(IResourceManager irm, IResource resource)
+        {
+            _resources = new Hashtable();
+            foreach (IResource rsc in _myResourceManager.Resources)
+            {
+                if (!(rsc is MaterialResourceItem))
+                    continue;
+                _resources.Add(((MaterialResourceItem)rsc).MaterialType, rsc);
+            }
+        }
+    }
 }
