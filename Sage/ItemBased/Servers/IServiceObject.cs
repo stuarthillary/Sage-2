05a06ff04a955fc5bf9cddc69acc8b978a28dd71
stuarthillary <shillary.git@gmail.com>
2021-03-14 17:18:56 +0000

    IServer cleanup

---
+++ Sage/ItemBased/Servers/IServiceObject.cs
@@ -0,0 +1,14 @@
+﻿/* This source code licensed under the GNU Affero General Public License */
+
+namespace Highpoint.Sage.ItemBased.Servers
+{
+    /// <summary>
+    /// Optional interface for a service object, in case it wants to be notified of its
+    /// stages of participation with, or processing by, a server.
+    /// </summary>
+    public interface IServiceObject
+    {
+        void OnServiceBeginning(IServer server);
+        void OnServiceCompleting(IServer server);
+    }
+}
