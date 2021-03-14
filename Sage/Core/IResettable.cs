2c77f192086e257e680e72be91733f7f8190eec4
stuarthillary <shillary.git@gmail.com>
2021-03-14 00:10:54 +0000

    IResettable cleanup

--- Sage/Core/IResettable.cs
+++ Sage/Core/IResettable.cs
@@ -1,10 +1,12 @@
 /* This source code licensed under the GNU Affero General Public License */
 
-namespace Highpoint.Sage.SimCore {
+namespace Highpoint.Sage.SimCore
+{
     /// <summary>
     /// Interface IResettable is implemented by any <see cref="IModelObject" /> that can be reset.
     /// </summary>
-    public interface IResettable {
+    public interface IResettable
+    {
         /// <summary>
         /// Performs a reset operation on this instance.
         /// </summary>
