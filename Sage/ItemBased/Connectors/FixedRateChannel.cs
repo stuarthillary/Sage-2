98e7a350e2fec19ce3dd51e2f92ede399f355273
stuarthillary <shillary.git@gmail.com>
2021-03-14 01:02:23 +0000

    FixedRateChannel cleanup

--- Sage/ItemBased/Connectors/FixedRateChannel.cs
+++ Sage/ItemBased/Connectors/FixedRateChannel.cs
@@ -14,16 +14,16 @@ namespace Highpoint.Sage.ItemBased.Channels
 	public class FixedRateChannel : IPortOwner {
 		
 		#region >>> Private Variables <<<
-		private SimpleInputPort m_entry;
-		private SimpleOutputPort m_exit;
-		private IExecutive m_exec;
-		private double m_capacity;
-		private DateTime m_lastExitArrivalTime;
-		private DateTime m_lastEntryAcceptanceTime;
-		private TimeSpan m_entryPeriod;
-		private TimeSpan m_transitPeriod;
-		private Queue m_queue;
-		private ExecEventReceiver m_dequeueEventHandler;
+		private readonly SimpleInputPort _entry;
+		private readonly SimpleOutputPort _exit;
+		private readonly IExecutive _exec;
+		private readonly double _capacity;
+		private DateTime _lastExitArrivalTime;
+		private DateTime _lastEntryAcceptanceTime;
+		private TimeSpan _entryPeriod;
+		private TimeSpan _transitPeriod;
+		private readonly Queue _queue;
+		private readonly ExecEventReceiver _dequeueEventHandler;
 		#endregion
 
         /// <summary>
@@ -37,43 +37,43 @@ namespace Highpoint.Sage.ItemBased.Channels
         /// <param name="transitPeriod">How long it takes an object to transit the channel.</param>
         /// <param name="capacity">How many objects the channel can hold.</param>
 		public FixedRateChannel(IModel model, string name, Guid guid, IExecutive exec, TimeSpan transitPeriod, double capacity) {
-			m_exec = exec;
-			m_transitPeriod = transitPeriod;
-			m_capacity = capacity;
-			m_entryPeriod = TimeSpan.FromTicks((long)((double)m_transitPeriod.Ticks/m_capacity));
-			m_queue = new Queue();
-			m_entry = new SimpleInputPort(model, "Entry", Guid.NewGuid(), this, new DataArrivalHandler(OnEntryAttempted));
-            m_exit = new SimpleOutputPort(model, "Exit", Guid.NewGuid(), this, new DataProvisionHandler(CantTakeFromChannel), new DataProvisionHandler(CantPeekFromChannel));
+			_exec = exec;
+			_transitPeriod = transitPeriod;
+			_capacity = capacity;
+			_entryPeriod = TimeSpan.FromTicks((long)((double)_transitPeriod.Ticks/_capacity));
+			_queue = new Queue();
+			_entry = new SimpleInputPort(model, "Entry", Guid.NewGuid(), this, new DataArrivalHandler(OnEntryAttempted));
+            _exit = new SimpleOutputPort(model, "Exit", Guid.NewGuid(), this, new DataProvisionHandler(CantTakeFromChannel), new DataProvisionHandler(CantPeekFromChannel));
             //m_ports.AddPort(m_entry); <-- Done in port's ctor.
             //m_ports.AddPort(m_exit); <-- Done in port's ctor.
-			m_dequeueEventHandler = new ExecEventReceiver(DequeueEventHandler);
+			_dequeueEventHandler = new ExecEventReceiver(DequeueEventHandler);
 		}
 
 		private bool OnEntryAttempted(object obj, IInputPort ip){
-			if ( m_queue.Count == 0 ) return AcceptEntry(obj);
-			if ( m_exec.Now-m_lastEntryAcceptanceTime >= m_entryPeriod ) return AcceptEntry(obj);
+			if ( _queue.Count == 0 ) return AcceptEntry(obj);
+			if ( _exec.Now-_lastEntryAcceptanceTime >= _entryPeriod ) return AcceptEntry(obj);
 			return false;			
 		}
 		private bool AcceptEntry(object obj){
 			TimeSpan forwardBuffer;
 			Bin bin;
-			if ( m_queue.Count == 0 ) {
-				forwardBuffer  = m_transitPeriod;
+			if ( _queue.Count == 0 ) {
+				forwardBuffer  = _transitPeriod;
 				bin = new Bin(obj,1.0,forwardBuffer);
 				ScheduleDequeueEvent(bin);
 			} else {
-				forwardBuffer = TimeSpan.FromTicks(Math.Max(m_transitPeriod.Ticks,(m_exec.Now.Ticks - m_lastEntryAcceptanceTime.Ticks)));
+				forwardBuffer = TimeSpan.FromTicks(Math.Max(_transitPeriod.Ticks,(_exec.Now.Ticks - _lastEntryAcceptanceTime.Ticks)));
 				bin = new Bin(obj,1.0,forwardBuffer);
 			}
-			m_lastEntryAcceptanceTime = m_exec.Now;
+			_lastEntryAcceptanceTime = _exec.Now;
 			return true;
 		}
 		private void ScheduleDequeueEvent(Bin bin){
-			m_exec.RequestEvent(m_dequeueEventHandler,m_exec.Now+bin.ForwardBuffer,0.0,bin);
+			_exec.RequestEvent(_dequeueEventHandler,_exec.Now+bin.ForwardBuffer,0.0,bin);
 		}
 		private void DequeueEventHandler(IExecutive exec, object bin){
-			m_lastExitArrivalTime = exec.Now;
-			m_exit.OwnerPut(((Bin)bin).Payload);
+			_lastExitArrivalTime = exec.Now;
+			_exit.OwnerPut(((Bin)bin).Payload);
 		}
 		private object CantTakeFromChannel(IOutputPort op, object selector){return null;}
         private object CantPeekFromChannel(IOutputPort op, object selector){ return null;}
@@ -81,23 +81,23 @@ namespace Highpoint.Sage.ItemBased.Channels
 		/// <summary>
 		/// The input port (i.e. the on-ramp).
 		/// </summary>
-		public IInputPort Entry { get { return m_entry; } }
+		public IInputPort Entry { get { return _entry; } }
 		/// <summary>
 		/// The output port (i.e. the off-ramp).
 		/// </summary>
-		public IOutputPort Exit { get { return m_exit;  } }
+		public IOutputPort Exit { get { return _exit;  } }
 
 		#region IPortOwner Implementation
 		/// <summary>
 		/// The PortSet object to which this IPortOwner delegates.
 		/// </summary>
-		private PortSet m_ports = new PortSet();
+		private readonly PortSet _ports = new PortSet();
 
 		/// <summary>
 		/// Registers a port with this IPortOwner
 		/// </summary>
 		/// <param name="port">The port that is to be added to this IPortOwner.</param>
-		public void AddPort(IPort port) {m_ports.AddPort(port);}
+		public void AddPort(IPort port) {_ports.AddPort(port);}
 
         /// <summary>
         /// Adds a port to this object's port set in the specified role or channel.
@@ -124,32 +124,32 @@ namespace Highpoint.Sage.ItemBased.Channels
         /// Unregisters a port from this IPortOwner.
         /// </summary>
         /// <param name="port">The port to be removed.</param>
-		public void RemovePort(IPort port){ m_ports.RemovePort(port); }
+		public void RemovePort(IPort port){ _ports.RemovePort(port); }
 		/// <summary>
 		/// Unregisters all ports that this IPortOwner knows to be its own.
 		/// </summary>
-		public void ClearPorts(){m_ports.ClearPorts();}
+		public void ClearPorts(){_ports.ClearPorts();}
 		/// <summary>
 		/// The public property that is the PortSet this IPortOwner owns.
 		/// </summary>
-		public IPortSet Ports { get { return m_ports; } }
+		public IPortSet Ports { get { return _ports; } }
 		#endregion
 
 		/// <summary>
 		/// A channel contains a series of bins. 
 		/// </summary>
 		private struct Bin {
-			private object m_payload;
-			private double m_capacity;
-			private TimeSpan m_forwardBuffer;
+			private readonly object _payload;
+			private readonly double _capacity;
+			private TimeSpan _forwardBuffer;
 			public Bin(object payload, double capacity, TimeSpan forwardBuffer){
-				m_payload = payload;
-				m_capacity = capacity;
-				m_forwardBuffer = forwardBuffer;
+				_payload = payload;
+				_capacity = capacity;
+				_forwardBuffer = forwardBuffer;
 			}
-			public object Payload { get { return m_payload; } }
-			public double Capacity { get { return m_capacity; } }
-			public TimeSpan ForwardBuffer { get { return m_forwardBuffer; } }
+			public object Payload { get { return _payload; } }
+			public double Capacity { get { return _capacity; } }
+			public TimeSpan ForwardBuffer { get { return _forwardBuffer; } }
 		}
 	}
 }
