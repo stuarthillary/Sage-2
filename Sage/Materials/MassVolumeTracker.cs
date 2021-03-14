b2673be55fb611a5c59b094130f52362e674cc3d
stuarthillary <shillary.git@gmail.com>
2021-03-14 23:57:50 +0000

    MassVolumeTracker cleanup

--- Sage/Materials/MassVolumeTracker.cs
+++ Sage/Materials/MassVolumeTracker.cs
@@ -1,9 +1,10 @@
 /* This source code licensed under the GNU Affero General Public License */
-using System;
 using Highpoint.Sage.Materials.Chemistry;
 using Highpoint.Sage.Utility;
+using System;
 
-namespace Highpoint.Sage.Materials {
+namespace Highpoint.Sage.Materials
+{
 
     /// <summary>
     /// Used to determine Mass and Volume high and low values during a change in mixture.
@@ -14,17 +15,18 @@ namespace Highpoint.Sage.Materials {
     /// Reactions may take place as a result of the charges.
     /// All mixtures passed in are cloned within this tracker before manipulation.
     /// </summary>
-    public class MassVolumeTracker {
+    public class MassVolumeTracker
+    {
 
         #region Private Fields
-        private double m_capacity;
-        private DoubleTracker m_massHistory;
-        private DoubleTracker m_volumeHistory;
-        private static readonly bool s_diagnostics = Diagnostics.DiagnosticAids.Diagnostics("MVTracker");
-        private Mixture m_initial, m_inflow, m_outflow;
-        private ReactionProcessor m_reactionProcessor;
-        private bool m_inflowFirst;
-        private bool m_cyclical;
+        private double _capacity;
+        private readonly DoubleTracker _massHistory;
+        private readonly DoubleTracker _volumeHistory;
+        private static readonly bool diagnostics = Diagnostics.DiagnosticAids.Diagnostics("MVTracker");
+        private Mixture _initial, _inflow, _outflow;
+        private readonly ReactionProcessor _reactionProcessor;
+        private readonly bool _inflowFirst;
+        private readonly bool _cyclical;
         #endregion Private Fields
 
         /// <summary>
@@ -56,27 +58,32 @@ namespace Highpoint.Sage.Materials {
         /// <param name="outflow">The outflowing mixture.</param>
         /// <param name="capacity">The capacity of the vessel.</param>
         /// <param name="rp">The ReactionProcessor that knows of any reactions that will take place. Can be null.</param>
-        public MassVolumeTracker(Mixture initial, Mixture inflow, Mixture outflow, double capacity, ReactionProcessor rp) {
-            m_massHistory = new DoubleTracker();
-            m_volumeHistory = new DoubleTracker();
+        public MassVolumeTracker(Mixture initial, Mixture inflow, Mixture outflow, double capacity, ReactionProcessor rp)
+        {
+            _massHistory = new DoubleTracker();
+            _volumeHistory = new DoubleTracker();
             SetInitialMixture(initial == null ? new Mixture() : (Mixture)initial.Clone());
             SetInflowMixture(inflow == null ? new Mixture() : (Mixture)inflow.Clone());
             SetOutflowMixture(outflow == null ? new Mixture() : (Mixture)outflow.Clone());
-            m_reactionProcessor = rp;
-            m_capacity = capacity;
-            m_cyclical = true;
-            m_inflowFirst = true;
+            _reactionProcessor = rp;
+            _capacity = capacity;
+            _cyclical = true;
+            _inflowFirst = true;
         }
 
         /// <summary>
         /// Sets the initial mixture in the modeled vessel.
         /// </summary>
         /// <param name="initial">The initial mixture in the modeled vessel.</param>
-        public void SetInitialMixture(Mixture initial) {
-            if (initial != null) {
-                m_initial = (Mixture)initial.Clone();
-            } else {
-                m_initial = new Mixture();
+        public void SetInitialMixture(Mixture initial)
+        {
+            if (initial != null)
+            {
+                _initial = (Mixture)initial.Clone();
+            }
+            else
+            {
+                _initial = new Mixture();
             }
         }
 
@@ -84,11 +91,15 @@ namespace Highpoint.Sage.Materials {
         /// Sets the inflowing mixture in the modeled vessel.
         /// </summary>
         /// <param name="inflow">The inflowing mixture in the modeled vessel.</param>
-        public void SetInflowMixture(Mixture inflow) {
-            if (inflow != null) {
-                m_inflow = (Mixture)inflow.Clone();
-            } else {
-                m_inflow = new Mixture();
+        public void SetInflowMixture(Mixture inflow)
+        {
+            if (inflow != null)
+            {
+                _inflow = (Mixture)inflow.Clone();
+            }
+            else
+            {
+                _inflow = new Mixture();
             }
         }
 
@@ -96,11 +107,15 @@ namespace Highpoint.Sage.Materials {
         /// Sets the outflowing mixture in the modeled vessel.
         /// </summary>
         /// <param name="outflow">The outflowing mixture in the modeled vessel.</param>
-        public void SetOutflowMixture(Mixture outflow) {
-            if (outflow != null) {
-                m_outflow = (Mixture)outflow.Clone();
-            } else {
-                m_outflow = new Mixture();
+        public void SetOutflowMixture(Mixture outflow)
+        {
+            if (outflow != null)
+            {
+                _outflow = (Mixture)outflow.Clone();
+            }
+            else
+            {
+                _outflow = new Mixture();
             }
         }
 
@@ -108,24 +123,26 @@ namespace Highpoint.Sage.Materials {
         /// Sets the volumetric capacity of the modeled vessel.
         /// </summary>
         /// <param name="capacity">The volumetric capacity of the modeled vessel.</param>
-        public void SetVesselCapacity(double capacity) {
-            m_capacity = capacity;
+        public void SetVesselCapacity(double capacity)
+        {
+            _capacity = capacity;
         }
 
         /// <summary>
         /// Performs the inflow/outflow cycle analysis. After this runs, consult the Mass, Volume and Temperature history members.
         /// </summary>
-        public void Process() {
+        public void Process()
+        {
 
-            m_massHistory.Reset();
-            m_volumeHistory.Reset();
+            _massHistory.Reset();
+            _volumeHistory.Reset();
 
-            Mixture contents = (Mixture)m_initial.Clone();
-            if (m_reactionProcessor != null)
-                m_reactionProcessor.Watch(contents);
+            Mixture contents = (Mixture)_initial.Clone();
+            if (_reactionProcessor != null)
+                _reactionProcessor.Watch(contents);
 
-            Mixture inflow = m_inflow==null?new Mixture():(Mixture)m_inflow.Clone();
-            Mixture outflow = m_outflow == null ? new Mixture() : (Mixture)m_outflow.Clone();
+            Mixture inflow = _inflow == null ? new Mixture() : (Mixture)_inflow.Clone();
+            Mixture outflow = _outflow == null ? new Mixture() : (Mixture)_outflow.Clone();
 
             double tiv = inflow.Volume;
             double tim = inflow.Mass;
@@ -137,30 +154,36 @@ namespace Highpoint.Sage.Materials {
             //if ( ( tiv + contents.Volume - tov > m_capacity ) ) useMinidumps = true;
 
             RecordMvt(contents);
-            if (m_cyclical) {
-                if (m_inflowFirst) {
-                    if (s_diagnostics)
+            if (_cyclical)
+            {
+                if (_inflowFirst)
+                {
+                    if (diagnostics)
                         Console.WriteLine("Initial - total volume = " + contents.Volume + ", mixture is " + contents);
-                    while (inflow.Mass > 0.0 || outflow.Mass > 0.0) {
+                    while (inflow.Mass > 0.0 || outflow.Mass > 0.0)
+                    {
 
                         // Perform charge
-                        if (inflow.Mass > 0.0) {
-                            double chgMass = Math.Min(( m_capacity - contents.Volume ) * ( tim / tiv ), inflow.Mass);
+                        if (inflow.Mass > 0.0)
+                        {
+                            double chgMass = Math.Min((_capacity - contents.Volume) * (tim / tiv), inflow.Mass);
                             double pctOfInFlow = chgMass / inflow.Mass;
                             contents.AddMaterial(inflow.RemoveMaterial(chgMass));
-                            if (s_diagnostics)
+                            if (diagnostics)
                                 Console.WriteLine("After charge - total volume = " + contents.Volume + ", mixture is " + contents);
                             RecordMvt(contents);
                         }
 
                         // Perform discharge
-                        if (outflow.Mass > 0.0) {
-                            double dischgMass = Math.Min(( m_capacity - contents.Volume ) * ( tom / tov ), outflow.Mass);
+                        if (outflow.Mass > 0.0)
+                        {
+                            double dischgMass = Math.Min((_capacity - contents.Volume) * (tom / tov), outflow.Mass);
                             Mixture extract = (Mixture)outflow.RemoveMaterial(dischgMass);
-                            foreach (Substance substance in extract.Constituents) {
+                            foreach (Substance substance in extract.Constituents)
+                            {
                                 contents.RemoveMaterial(substance.MaterialType, extract.Mass);
                             }
-                            if (s_diagnostics)
+                            if (diagnostics)
                                 Console.WriteLine("After discharge - total volume = " + contents.Volume + ", mixture is " + contents);
                             RecordMvt(contents);
                         }
@@ -168,31 +191,48 @@ namespace Highpoint.Sage.Materials {
                         if (useMinidumps)
                             contents.Clear();
                     }
-                } else {
+                }
+                else
+                {
                     throw new NotImplementedException("Only able to model inflow-first, cyclical behavior.");
                 }
-            } else {
+            }
+            else
+            {
                 throw new NotImplementedException("Only able to model inflow-first, cyclical behavior.");
             }
-            if (m_reactionProcessor != null)
-                m_reactionProcessor.Ignore(contents);
+            if (_reactionProcessor != null)
+                _reactionProcessor.Ignore(contents);
         }
 
-        private void RecordMvt(Mixture mixture) {
-            m_massHistory.Register(mixture.Mass);
-            m_volumeHistory.Register(mixture.Volume);
+        private void RecordMvt(Mixture mixture)
+        {
+            _massHistory.Register(mixture.Mass);
+            _volumeHistory.Register(mixture.Volume);
             //m_temperatureHistory.Register(mixture.Temperature);
         }
 
         /// <summary>
         /// DoubleTracker that provides the initial, min, max and final mass values of the mixture in the vessel.
         /// </summary>
-        public DoubleTracker MassHistory { get { return m_massHistory; } }
+        public DoubleTracker MassHistory
+        {
+            get
+            {
+                return _massHistory;
+            }
+        }
 
         /// <summary>
         /// DoubleTracker that provides the initial, min, max and final volume values of the mixture in the vessel.
         /// </summary>
-        public DoubleTracker VolumeHistory { get { return m_volumeHistory; } }
+        public DoubleTracker VolumeHistory
+        {
+            get
+            {
+                return _volumeHistory;
+            }
+        }
 
         //		/// <summary>
         //		/// DoubleTracker that provides the initial, min, max and final temperature values of the mixture in the vessel.
