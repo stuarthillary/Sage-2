dc05df954e2102491a91efb12f45b641ac338d35
stuarthillary <shillary.git@gmail.com>
2021-03-15 10:31:58 +0000

    TimePeriodSorter etc cleanup

--- Sage/Scheduling/TimePeriodPart.cs
+++ Sage/Scheduling/TimePeriodPart.cs
@@ -1,25 +1,27 @@
 /* This source code licensed under the GNU Affero General Public License */
 
-namespace Highpoint.Sage.Scheduling {
+namespace Highpoint.Sage.Scheduling
+{
 
-	/// <summary>
-	/// Declares the property that is being changed in a TimePeriodAdjustment. By
-	/// examining the TimeAdjustmentMode of the target time period, the user can
-	/// determine what changed/will change.
-	/// </summary>
-	public enum TimePeriodPart { 
+    /// <summary>
+    /// Declares the property that is being changed in a TimePeriodAdjustment. By
+    /// examining the TimeAdjustmentMode of the target time period, the user can
+    /// determine what changed/will change.
+    /// </summary>
+    public enum TimePeriodPart
+    {
         /// <summary>
         /// The start time is being changed. 
         /// </summary>
-        StartTime, 
+        StartTime,
         /// <summary>
         /// The duration is being changed.
         /// </summary>
-        Duration, 
+        Duration,
         /// <summary>
         /// The end time is being changed.
         /// </summary>
-        EndTime 
+        EndTime
     }
-	
+
 }
