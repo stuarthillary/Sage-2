eb37b9908a5405d4ec1dd3b08d29f438a5b384d1
stuarthillary <shillary.git@gmail.com>
2021-03-15 12:24:39 +0000

    SerialNumber cleanup

---
+++ Sage/Utility/IHasSerialNumber.cs
@@ -0,0 +1,18 @@
+﻿/* This source code licensed under the GNU Affero General Public License */
+
+namespace Highpoint.Sage.Utility
+{
+    /// <summary>
+    /// Implemented by an object that has a serial number.
+    /// </summary>
+    public interface IHasSerialNumber
+    {
+        /// <summary>
+        /// The object's serial number.
+        /// </summary>
+        long SerialNumber
+        {
+            get;
+        }
+    }
+}
