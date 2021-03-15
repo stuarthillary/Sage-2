d73f6e5167b6b0ab7a6e380d97f9f9cb6197d564
stuarthillary <shillary.git@gmail.com>
2021-03-15 13:39:16 +0000

    TestBranchBlocks cleanup

--- Sage_Aux/SageTestLib/TestBranchBlocks.cs
+++ Sage_Aux/SageTestLib/TestBranchBlocks.cs
@@ -1,81 +1,94 @@
 /* This source code licensed under the GNU Affero General Public License */
-using System;
-using System.Diagnostics;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Highpoint.Sage.SimCore;
 using Highpoint.Sage.ItemBased.Ports;
 using Highpoint.Sage.ItemBased.SplittersAndJoiners;
+using Highpoint.Sage.SimCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Diagnostics;
 
-namespace Highpoint.Sage.ItemBased {
-	using StochTwoChoice = SimpleStochasticTwoChoiceBranchBlock;
-	using DelegTwoChoice = SimpleDelegatedTwoChoiceBranchBlock;
+namespace Highpoint.Sage.ItemBased
+{
+    using DelegTwoChoice = SimpleDelegatedTwoChoiceBranchBlock;
+    using StochTwoChoice = SimpleStochasticTwoChoiceBranchBlock;
 
-	/// <summary>
-	/// Summary description for zTestBranchBlocks.
-	/// </summary>
-	[TestClass]
-	public class BranchBlockTester {
+    /// <summary>
+    /// Summary description for zTestBranchBlocks.
+    /// </summary>
+    [TestClass]
+    public class BranchBlockTester
+    {
 
-		#region MSTest Goo
-		[TestInitialize] 
-		public void Init() {
-		}
-		[TestCleanup]
-		public void destroy() {
-			Debug.WriteLine( "Done." );
-		}
-		#endregion
+        #region MSTest Goo
+        [TestInitialize]
+        public void Init()
+        {
+        }
+        [TestCleanup]
+        public void destroy()
+        {
+            Debug.WriteLine("Done.");
+        }
+        #endregion
 
-		int lastResult = -1;
-		public BranchBlockTester() {}
+        int _lastResult = -1;
+        public BranchBlockTester()
+        {
+        }
 
-		private int [] m_expected = new int[]{1,1,0,1,0,1,0,0,1,1,1,1,1,0,1,1,0,0,0,1,0,1,1,0,1,1,1,0,1,1,1,1,0,
-		                                      1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,0,1,1,0,0,1,1,1,1,1,1,0,1,1,1,1,0,1,
-		                                      1,0,0,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,};
-		private int m_itemNumber;
+        private readonly int[] _expected = new int[]{1,1,0,1,0,1,0,0,1,1,1,1,1,0,1,1,0,0,0,1,0,1,1,0,1,1,1,0,1,1,1,1,0,
+                                              1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,0,1,1,0,0,1,1,1,1,1,1,0,1,1,1,1,0,1,
+                                              1,0,0,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,};
+        private int _itemNumber;
 
-		[TestMethod]
-		public void TestStochasticBranchBlock(){
-			Model model = new Model();
-		    model.RandomServer = new Randoms.RandomServer(12345, 100);
+        [TestMethod]
+        public void TestStochasticBranchBlock()
+        {
+            Model model = new Model();
+            model.RandomServer = new Randoms.RandomServer(12345, 100);
 
-            StochTwoChoice ss2cbb = new StochTwoChoice(model,"s2c",Guid.NewGuid(),.2);
-			ss2cbb.Outputs[0].PortDataPresented+=new PortDataEvent(Out0_PortDataPresented);
-			ss2cbb.Outputs[1].PortDataPresented+=new PortDataEvent(Out1_PortDataPresented);
-	
-	        for ( m_itemNumber = 0 ; m_itemNumber < m_expected.Length ; m_itemNumber++ ) {
-				ss2cbb.Input.Put(new object());
-				Debug.Write(lastResult + ",");
-                Assert.IsTrue(lastResult == m_expected[m_itemNumber],"Unexpected choice.");
-			}
-		}
+            StochTwoChoice ss2cbb = new StochTwoChoice(model, "s2c", Guid.NewGuid(), .2);
+            ss2cbb.Outputs[0].PortDataPresented += new PortDataEvent(Out0_PortDataPresented);
+            ss2cbb.Outputs[1].PortDataPresented += new PortDataEvent(Out1_PortDataPresented);
 
-		[TestMethod]
-		public void TestDelegatedBranchBlock(){
-			Model model = new Model();
-			DelegTwoChoice d2cbb = new DelegTwoChoice(model,"s2c",Guid.NewGuid());
-			d2cbb.BooleanDeciderDelegate = new BooleanDecider(ChooseYesOrNo);
-			d2cbb.Outputs[0].PortDataPresented+=new PortDataEvent(Out0_PortDataPresented);
-			d2cbb.Outputs[1].PortDataPresented+=new PortDataEvent(Out1_PortDataPresented);
-			Randoms.RandomServer rs = new Randoms.RandomServer(12345,100);
-			model.RandomServer = rs;
-			for ( m_itemNumber = 0 ; m_itemNumber < m_expected.Length ; m_itemNumber++ ) {
-				d2cbb.Input.Put(new object());
-				Debug.Write(lastResult + ",");
-                Assert.IsTrue(lastResult == m_expected[m_itemNumber],"Unexpected choice.");
-			}
-		}
+            for (_itemNumber = 0; _itemNumber < _expected.Length; _itemNumber++)
+            {
+                ss2cbb.Input.Put(new object());
+                Debug.Write(_lastResult + ",");
+                Assert.IsTrue(_lastResult == _expected[_itemNumber], "Unexpected choice.");
+            }
+        }
 
-		private bool ChooseYesOrNo(object serverObject){
-			return m_expected[m_itemNumber]==0;
-		}
+        [TestMethod]
+        public void TestDelegatedBranchBlock()
+        {
+            Model model = new Model();
+            DelegTwoChoice d2cbb = new DelegTwoChoice(model, "s2c", Guid.NewGuid());
+            d2cbb.BooleanDeciderDelegate = new BooleanDecider(ChooseYesOrNo);
+            d2cbb.Outputs[0].PortDataPresented += new PortDataEvent(Out0_PortDataPresented);
+            d2cbb.Outputs[1].PortDataPresented += new PortDataEvent(Out1_PortDataPresented);
+            Randoms.RandomServer rs = new Randoms.RandomServer(12345, 100);
+            model.RandomServer = rs;
+            for (_itemNumber = 0; _itemNumber < _expected.Length; _itemNumber++)
+            {
+                d2cbb.Input.Put(new object());
+                Debug.Write(_lastResult + ",");
+                Assert.IsTrue(_lastResult == _expected[_itemNumber], "Unexpected choice.");
+            }
+        }
 
-		private void Out0_PortDataPresented(object data, IPort where) {
-			lastResult = 0;
-		}
+        private bool ChooseYesOrNo(object serverObject)
+        {
+            return _expected[_itemNumber] == 0;
+        }
 
-		private void Out1_PortDataPresented(object data, IPort where) {
-			lastResult = 1;
-		}
-	}
+        private void Out0_PortDataPresented(object data, IPort where)
+        {
+            _lastResult = 0;
+        }
+
+        private void Out1_PortDataPresented(object data, IPort where)
+        {
+            _lastResult = 1;
+        }
+    }
 }
