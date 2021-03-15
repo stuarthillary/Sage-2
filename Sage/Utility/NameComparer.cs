34945976dbd7e62078a5d0c893fa82e90f941375
stuarthillary <shillary.git@gmail.com>
2021-03-15 12:22:33 +0000

    NameComparer etc cleanup

--- Sage/Utility/NameComparer.cs
+++ Sage/Utility/NameComparer.cs
@@ -1,8 +1,10 @@
 /* This source code licensed under the GNU Affero General Public License */
 using System.Collections.Generic;
 
-namespace Highpoint.Sage.Utility {
-    public class NameComparer<T> : IComparer<T> where T : SimCore.IHasName {
+namespace Highpoint.Sage.Utility
+{
+    public class NameComparer<T> : IComparer<T> where T : SimCore.IHasName
+    {
         #region IComparer<T> Members
         /// <summary>
         /// Compares two objects and returns a value indicating whether the name of one is less than, equal to, or greater than the name of the other.
@@ -10,7 +12,8 @@ namespace Highpoint.Sage.Utility {
         /// <param name="x">The first object to compare.</param>
         /// <param name="y">The second object to compare.</param>
         /// <returns>A signed integer that indicates the relative values of <paramref name="x" /> and <paramref name="y" />, as shown in the following table.Value Meaning Less than zero<paramref name="x" /> is less than <paramref name="y" />.Zero<paramref name="x" /> equals <paramref name="y" />.Greater than zero<paramref name="x" /> is greater than <paramref name="y" />.</returns>
-        public int Compare(T x, T y) {
+        public int Compare(T x, T y)
+        {
             return System.Collections.Comparer.Default.Compare(x.Name, y.Name);
         }
         #endregion
