2114b10edf65bda44deefb0a39538f04bf34a4dd
stuarthillary <shillary.git@gmail.com>
2021-03-15 12:18:17 +0000

    LabelManager cleanup

---
+++ Sage/Utility/IHasLabel.cs
@@ -0,0 +1,33 @@
+﻿/* This source code licensed under the GNU Affero General Public License */
+
+namespace Highpoint.Sage.Utility
+{
+    /// <summary>
+    /// Implemented by an object that is capable of managing context-specific labels.
+    /// </summary>
+    public interface IHasLabel
+    {
+
+        /// <summary>
+        /// Gets or sets the label in the currently-selected context, or if none has been selected, then according to the default context.
+        /// </summary>
+        /// <value>The label.</value>
+        string Label
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// Sets the label in the context indicated by the provided context, or if null or String.Empty has been selected, then in the default context.
+        /// </summary>
+        /// <param name="label">The label.</param>
+        /// <param name="context">The context - use null or string.Empty for the default context.</param>
+        void SetLabel(string label, string context);
+
+        /// <summary>
+        /// Gets the label from the context indicated by the provided context, or if null or String.Empty has been selected, then from the default context.
+        /// </summary>
+        /// <param name="context">The context - use null or string.Empty for the default context.</param>
+        string GetLabel(string context);
+    }
+}
--- Sage/Utility/LabelManager.cs
+++ Sage/Utility/LabelManager.cs
@@ -3,39 +3,16 @@ using System;
 using System.Collections.Generic;
 using System.Threading;
 
-namespace Highpoint.Sage.Utility {
+namespace Highpoint.Sage.Utility
+{
 
-    /// <summary>
-    /// Implemented by an object that is capable of managing context-specific labels.
-    /// </summary>
-    public interface IHasLabel {
-
-        /// <summary>
-        /// Gets or sets the label in the currently-selected context, or if none has been selected, then according to the default context.
-        /// </summary>
-        /// <value>The label.</value>
-        string Label { get; set; }
-
-        /// <summary>
-        /// Sets the label in the context indicated by the provided context, or if null or String.Empty has been selected, then in the default context.
-        /// </summary>
-        /// <param name="label">The label.</param>
-        /// <param name="context">The context - use null or string.Empty for the default context.</param>
-        void SetLabel(string label, string context);
-
-        /// <summary>
-        /// Gets the label from the context indicated by the provided context, or if null or String.Empty has been selected, then from the default context.
-        /// </summary>
-        /// <param name="context">The context - use null or string.Empty for the default context.</param>
-        string GetLabel(string context);
-    }
-    
     /// <summary>
     /// A manager to which a class that implements a Label can delegate.
     /// </summary>
-    public class LabelManager : IHasLabel {
+    public class LabelManager : IHasLabel
+    {
 
-        private static readonly LocalDataStoreSlot s_ldss;
+        private static readonly LocalDataStoreSlot _ldss;
         /// <summary>
         /// The name of the Thread-Local-Storage data slot in which the current context key is stored.
         /// </summary>
@@ -46,29 +23,32 @@ namespace Highpoint.Sage.Utility {
         /// </summary>
         public static readonly string DEFAULT_CHANNEL = "SageDefaultLabelContext";
 
-        private readonly Dictionary<string, string> m_labels;
+        private readonly Dictionary<string, string> _labels;
 
         /// <summary>
         /// Initializes the <see cref="T:LabelManager"/> class by creating a Thread-Local-Storage data slot for the key.
         /// </summary>
-        static LabelManager() {
-            s_ldss = Thread.AllocateNamedDataSlot(CONTEXTSLOTNAME);
+        static LabelManager()
+        {
+            _ldss = Thread.AllocateNamedDataSlot(CONTEXTSLOTNAME);
         }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="T:LabelManager"/> class.
         /// </summary>
-        public LabelManager() {
-            m_labels = new Dictionary<string, string> {[DEFAULT_CHANNEL] = String.Empty};
+        public LabelManager()
+        {
+            _labels = new Dictionary<string, string> { [DEFAULT_CHANNEL] = String.Empty };
         }
 
         /// <summary>
         /// Sets the label context for all unspecified requests in this thread.
         /// </summary>
         /// <param name="context">The context.</param>
-        public static void SetContext(string context) {
+        public static void SetContext(string context)
+        {
             LocalDataStoreSlot ldss = Thread.GetNamedDataSlot(CONTEXTSLOTNAME);
-            Thread.SetData(ldss,context);
+            Thread.SetData(ldss, context);
         }
 
         #region IHasLabel Members
@@ -77,19 +57,28 @@ namespace Highpoint.Sage.Utility {
         /// Gets or sets the label in the currently-selected context, or if none has been selected, then according to the default context.
         /// </summary>
         /// <value>The label.</value>
-        public string Label {
-            get {
-                if (m_labels.ContainsKey(Key)) {
-                    return m_labels[Key];
-                } else {
+        public string Label
+        {
+            get
+            {
+                if (_labels.ContainsKey(Key))
+                {
+                    return _labels[Key];
+                }
+                else
+                {
                     return string.Empty;
                 }
             }
-            set {
-                if (m_labels.ContainsKey(Key)) {
-                    m_labels[Key] = value;
-                } else {
-                    m_labels.Add(Key, value);
+            set
+            {
+                if (_labels.ContainsKey(Key))
+                {
+                    _labels[Key] = value;
+                }
+                else
+                {
+                    _labels.Add(Key, value);
                 }
             }
         }
@@ -99,11 +88,13 @@ namespace Highpoint.Sage.Utility {
         /// </summary>
         /// <param name="label">The label.</param>
         /// <param name="context">The context - use null or string.Empty for the default context.</param>
-        public void SetLabel(string label, string context) {
-            if (context == null || context.Equals(string.Empty)) {
+        public void SetLabel(string label, string context)
+        {
+            if (context == null || context.Equals(string.Empty))
+            {
                 context = DEFAULT_CHANNEL;
             }
-            m_labels[context] = label;
+            _labels[context] = label;
         }
 
         /// <summary>
@@ -111,11 +102,13 @@ namespace Highpoint.Sage.Utility {
         /// </summary>
         /// <param name="context">The context - use null or string.Empty for the default context.</param>
         /// <returns></returns>
-        public string GetLabel(string context) {
-            if (context == null || context.Equals(string.Empty)) {
+        public string GetLabel(string context)
+        {
+            if (context == null || context.Equals(string.Empty))
+            {
                 context = DEFAULT_CHANNEL;
             }
-            return m_labels[context];
+            return _labels[context];
         }
 
         #endregion
@@ -124,6 +117,6 @@ namespace Highpoint.Sage.Utility {
         /// Gets the key.
         /// </summary>
         /// <value>The key.</value>
-        private string Key => (string)Thread.GetData(s_ldss) ?? DEFAULT_CHANNEL;
+        private string Key => (string)Thread.GetData(_ldss) ?? DEFAULT_CHANNEL;
     }
 }
