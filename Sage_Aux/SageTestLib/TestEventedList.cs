44989bed431bd8ed5269dbfb9ee9a708588ec55a
stuarthillary <shillary.git@gmail.com>
2021-03-18 23:31:11 +0000

    TestEventedList cleanup

--- Sage_Aux/SageTestLib/TestEventedList.cs
+++ Sage_Aux/SageTestLib/TestEventedList.cs
@@ -1,178 +1,205 @@
 /* This source code licensed under the GNU Affero General Public License */
+using Highpoint.Sage.Utility;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Diagnostics;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Highpoint.Sage.Utility;
 
-namespace SageTestLib {
+namespace SageTestLib
+{
     [TestClass]
-    public class EventedListTester {
+    public class EventedListTester
+    {
 
-        public EventedListTester() {}
+        public EventedListTester()
+        {
+        }
 
-        EventedList<string> m_uut = null;
-        string m_responses = null;
-       
+        private EventedList<string> _uut = null;
+        private string _responses = null;
+
         #region Prep Work
 
-        public void Init() {
-            m_responses = "";
-            m_uut = new EventedList<string>();
-            m_uut.AboutToAddItem += m_uut_AboutToAddItem;
-            m_uut.AboutToAddItems += m_uut_AboutToAddItems;
-            m_uut.AboutToRemoveItem += m_uut_AboutToRemoveItem;
-            m_uut.AboutToRemoveItems += m_uut_AboutToRemoveItems;
-            m_uut.AboutToReplaceItem += m_uut_AboutToReplaceItem;
-            m_uut.AddedItem += m_uut_AddedItem;
-            m_uut.AddedItems += m_uut_AddedItems;
-            m_uut.RemovedItem += m_uut_RemovedItem;
-            m_uut.RemovedItems += m_uut_RemovedItems;
-            m_uut.ReplacedItem += m_uut_ReplacedItem;
-            m_uut.ContentsChanged += m_uut_ContentsChanged;
+        public void Init()
+        {
+            _responses = "";
+            _uut = new EventedList<string>();
+            _uut.AboutToAddItem += m_uut_AboutToAddItem;
+            _uut.AboutToAddItems += m_uut_AboutToAddItems;
+            _uut.AboutToRemoveItem += m_uut_AboutToRemoveItem;
+            _uut.AboutToRemoveItems += m_uut_AboutToRemoveItems;
+            _uut.AboutToReplaceItem += m_uut_AboutToReplaceItem;
+            _uut.AddedItem += m_uut_AddedItem;
+            _uut.AddedItems += m_uut_AddedItems;
+            _uut.RemovedItem += m_uut_RemovedItem;
+            _uut.RemovedItems += m_uut_RemovedItems;
+            _uut.ReplacedItem += m_uut_ReplacedItem;
+            _uut.ContentsChanged += m_uut_ContentsChanged;
         }
 
-        void m_uut_ReplacedItem(EventedList<string> list, string oldItem, string newItem) {
-            m_responses += "m_uut_ReplacedItem" + " " + oldItem + " with " + newItem + " | ";
+        void m_uut_ReplacedItem(EventedList<string> list, string oldItem, string newItem)
+        {
+            _responses += "m_uut_ReplacedItem" + " " + oldItem + " with " + newItem + " | ";
         }
 
-        void m_uut_AboutToReplaceItem(EventedList<string> list, string oldItem, string newItem) {
-            m_responses += "m_uut_AboutToReplaceItem" + " " + oldItem + " with " + newItem + " | ";
+        void m_uut_AboutToReplaceItem(EventedList<string> list, string oldItem, string newItem)
+        {
+            _responses += "m_uut_AboutToReplaceItem" + " " + oldItem + " with " + newItem + " | ";
         }
 
-        void m_uut_ContentsChanged(EventedList<string> list) {
-            m_responses += "m_uut_ContentsChanged" + " | ";
+        void m_uut_ContentsChanged(EventedList<string> list)
+        {
+            _responses += "m_uut_ContentsChanged" + " | ";
         }
 
-        void m_uut_RemovedItems(EventedList<string> list, Predicate<string> match) {
-            m_responses += "m_uut_RemovedItems" + " " + match + " | ";
+        void m_uut_RemovedItems(EventedList<string> list, Predicate<string> match)
+        {
+            _responses += "m_uut_RemovedItems" + " " + match + " | ";
         }
 
-        void m_uut_RemovedItem(EventedList<string> list, string item) {
-            m_responses += "m_uut_RemovedItem" + " " + item + " | ";
+        void m_uut_RemovedItem(EventedList<string> list, string item)
+        {
+            _responses += "m_uut_RemovedItem" + " " + item + " | ";
         }
 
-        void m_uut_AddedItems(EventedList<string> list, System.Collections.Generic.IEnumerable<string> collection) {
-            m_responses += "m_uut_AddedItems" + " " + collection + " | ";
+        void m_uut_AddedItems(EventedList<string> list, System.Collections.Generic.IEnumerable<string> collection)
+        {
+            _responses += "m_uut_AddedItems" + " " + collection + " | ";
         }
 
-        void m_uut_AddedItem(EventedList<string> list, string item) {
-            m_responses += "m_uut_AddedItem" + " " + item + " | ";
+        void m_uut_AddedItem(EventedList<string> list, string item)
+        {
+            _responses += "m_uut_AddedItem" + " " + item + " | ";
         }
 
-        void m_uut_AboutToRemoveItems(EventedList<string> list, Predicate<string> match) {
-            m_responses += "m_uut_AboutToRemoveItems" + " " + match + " | ";
+        void m_uut_AboutToRemoveItems(EventedList<string> list, Predicate<string> match)
+        {
+            _responses += "m_uut_AboutToRemoveItems" + " " + match + " | ";
         }
 
-        void m_uut_AboutToRemoveItem(EventedList<string> list, string item) {
-            m_responses += "m_uut_AboutToRemoveItem" + " " + item + " | ";
+        void m_uut_AboutToRemoveItem(EventedList<string> list, string item)
+        {
+            _responses += "m_uut_AboutToRemoveItem" + " " + item + " | ";
         }
 
-        void m_uut_AboutToAddItems(EventedList<string> list, System.Collections.Generic.IEnumerable<string> collection) {
-            m_responses += "m_uut_AboutToAddItems" + " " + collection + " | ";
+        void m_uut_AboutToAddItems(EventedList<string> list, System.Collections.Generic.IEnumerable<string> collection)
+        {
+            _responses += "m_uut_AboutToAddItems" + " " + collection + " | ";
         }
 
-        void m_uut_AboutToAddItem(EventedList<string> list, string item) {
-            m_responses += "m_uut_AboutToAddItem" + " " + item + " | ";
+        void m_uut_AboutToAddItem(EventedList<string> list, string item)
+        {
+            _responses += "m_uut_AboutToAddItem" + " " + item + " | ";
         }
 
         [TestCleanup]
-        public void destroy() {
+        public void destroy()
+        {
             Debug.WriteLine("Done.");
-        } 
+        }
         #endregion
 
         [TestMethod]
         [Highpoint.Sage.Utility.FieldDescription("Test the Add mechanism.")]
-        public void TestAdd() {
+        public void TestAdd()
+        {
             Init();
 
             string addee = "String 1";
-            m_uut.Add(addee);
+            _uut.Add(addee);
 
-            Assert.IsTrue(m_responses.Equals("m_uut_AboutToAddItem String 1 | m_uut_AddedItem String 1 | m_uut_ContentsChanged | "));
-            Console.WriteLine(m_responses);
+            Assert.IsTrue(_responses.Equals("m_uut_AboutToAddItem String 1 | m_uut_AddedItem String 1 | m_uut_ContentsChanged | "));
+            Console.WriteLine(_responses);
         }
-        
+
         [TestMethod]
         [Highpoint.Sage.Utility.FieldDescription("Test the AddRange mechanism.")]
-        public void TestAddRange() {
+        public void TestAddRange()
+        {
             Init();
 
             string[] addee = new string[] { "String 2", "String 3" };
-            m_uut.AddRange(addee);
+            _uut.AddRange(addee);
 
-            Assert.IsTrue(m_responses.Equals("m_uut_AboutToAddItems System.String[] | m_uut_AddedItems System.String[] | m_uut_ContentsChanged | "));
-            Assert.IsTrue(m_uut[0].Equals("String 2"));
-            Assert.IsTrue(m_uut[1].Equals("String 3"));
-            Console.WriteLine(m_responses);
+            Assert.IsTrue(_responses.Equals("m_uut_AboutToAddItems System.String[] | m_uut_AddedItems System.String[] | m_uut_ContentsChanged | "));
+            Assert.IsTrue(_uut[0].Equals("String 2"));
+            Assert.IsTrue(_uut[1].Equals("String 3"));
+            Console.WriteLine(_responses);
         }
-        
+
         [TestMethod]
         [Highpoint.Sage.Utility.FieldDescription("Test the Remove mechanism.")]
-        public void TestRemove() {
+        public void TestRemove()
+        {
             Init();
 
-            m_uut.AddRange(new string[] { "Bob", "Mary", "Sue" });
-            m_uut.Remove("Mary");
+            _uut.AddRange(new string[] { "Bob", "Mary", "Sue" });
+            _uut.Remove("Mary");
 
-            Assert.IsTrue(m_uut[0].Equals("Bob"));
-            Assert.IsTrue(m_uut[1].Equals("Sue"));
-            Assert.IsTrue(m_responses.Equals("m_uut_AboutToAddItems System.String[] | m_uut_AddedItems System.String[] | m_uut_ContentsChanged | m_uut_AboutToRemoveItem Mary | m_uut_RemovedItem Mary | m_uut_ContentsChanged | "));
-            Console.WriteLine(m_responses);
+            Assert.IsTrue(_uut[0].Equals("Bob"));
+            Assert.IsTrue(_uut[1].Equals("Sue"));
+            Assert.IsTrue(_responses.Equals("m_uut_AboutToAddItems System.String[] | m_uut_AddedItems System.String[] | m_uut_ContentsChanged | m_uut_AboutToRemoveItem Mary | m_uut_RemovedItem Mary | m_uut_ContentsChanged | "));
+            Console.WriteLine(_responses);
         }
 
         [TestMethod]
         [Highpoint.Sage.Utility.FieldDescription("Test the RemoveAll mechanism.")]
-        public void TestRemoveAll() {
+        public void TestRemoveAll()
+        {
             Init();
 
-            m_uut.AddRange(new string[] { "Bob", "Mary", "Sue" });
-            m_uut.RemoveAll(delegate(string s) { return s.Length.Equals(3); });
+            _uut.AddRange(new string[] { "Bob", "Mary", "Sue" });
+            _uut.RemoveAll(delegate (string s)
+            {
+                return s.Length.Equals(3);
+            });
 
-            Assert.IsTrue(m_uut[0].Equals("Mary"));
-            Assert.IsTrue(m_responses.Equals("m_uut_AboutToAddItems System.String[] | m_uut_AddedItems System.String[] | m_uut_ContentsChanged | m_uut_AboutToRemoveItems System.Predicate`1[System.String] | m_uut_RemovedItems System.Predicate`1[System.String] | m_uut_ContentsChanged | "));
-            Console.WriteLine(m_responses);
+            Assert.IsTrue(_uut[0].Equals("Mary"));
+            Assert.IsTrue(_responses.Equals("m_uut_AboutToAddItems System.String[] | m_uut_AddedItems System.String[] | m_uut_ContentsChanged | m_uut_AboutToRemoveItems System.Predicate`1[System.String] | m_uut_RemovedItems System.Predicate`1[System.String] | m_uut_ContentsChanged | "));
+            Console.WriteLine(_responses);
         }
 
         [TestMethod]
         [Highpoint.Sage.Utility.FieldDescription("Test the RemoveAt mechanism.")]
-        public void TestRemoveAt() {
+        public void TestRemoveAt()
+        {
             Init();
 
-            m_uut.AddRange(new string[] { "Bob", "Mary", "Sue" });
-            m_uut.RemoveAt(1);
+            _uut.AddRange(new string[] { "Bob", "Mary", "Sue" });
+            _uut.RemoveAt(1);
 
-            Assert.IsTrue(m_uut[0].Equals("Bob"));
-            Assert.IsTrue(m_uut[1].Equals("Sue"));
-            Assert.IsTrue(m_responses.Equals("m_uut_AboutToAddItems System.String[] | m_uut_AddedItems System.String[] | m_uut_ContentsChanged | m_uut_AboutToRemoveItem Mary | m_uut_RemovedItem Mary | m_uut_ContentsChanged | "));
-            Console.WriteLine(m_responses);
+            Assert.IsTrue(_uut[0].Equals("Bob"));
+            Assert.IsTrue(_uut[1].Equals("Sue"));
+            Assert.IsTrue(_responses.Equals("m_uut_AboutToAddItems System.String[] | m_uut_AddedItems System.String[] | m_uut_ContentsChanged | m_uut_AboutToRemoveItem Mary | m_uut_RemovedItem Mary | m_uut_ContentsChanged | "));
+            Console.WriteLine(_responses);
         }
 
         [TestMethod]
         [Highpoint.Sage.Utility.FieldDescription("Test the Clear mechanism.")]
-        public void TestClear() {
+        public void TestClear()
+        {
             Init();
 
-            m_uut.AddRange(new string[] { "Bob", "Mary", "Sue" });
-            m_uut.Clear();
+            _uut.AddRange(new string[] { "Bob", "Mary", "Sue" });
+            _uut.Clear();
 
-            Assert.IsTrue(m_uut.Count == 0);
-            Assert.IsTrue(m_responses.Equals("m_uut_AboutToAddItems System.String[] | m_uut_AddedItems System.String[] | m_uut_ContentsChanged | m_uut_ContentsChanged | "));
-            Console.WriteLine(m_responses);
+            Assert.IsTrue(_uut.Count == 0);
+            Assert.IsTrue(_responses.Equals("m_uut_AboutToAddItems System.String[] | m_uut_AddedItems System.String[] | m_uut_ContentsChanged | m_uut_ContentsChanged | "));
+            Console.WriteLine(_responses);
         }
 
         [TestMethod]
         [Highpoint.Sage.Utility.FieldDescription("Test the Indexer mechanism.")]
-        public void TestIndexer() {
+        public void TestIndexer()
+        {
             Init();
 
-            m_uut.AddRange(new string[] { "Bob", "Mary", "Sue" });
-            m_uut[1] = "Steve";
+            _uut.AddRange(new string[] { "Bob", "Mary", "Sue" });
+            _uut[1] = "Steve";
 
-            Assert.AreEqual("Bob",m_uut[0]);
-            Assert.AreEqual("Steve",m_uut[1]);
-            Assert.AreEqual("Sue",m_uut[2]);
-            Assert.AreEqual("m_uut_AboutToAddItems System.String[] | m_uut_AddedItems System.String[] | m_uut_ContentsChanged | m_uut_AboutToReplaceItem Mary with Steve | m_uut_ReplacedItem Mary with Steve | m_uut_ContentsChanged | ", m_responses);
+            Assert.AreEqual("Bob", _uut[0]);
+            Assert.AreEqual("Steve", _uut[1]);
+            Assert.AreEqual("Sue", _uut[2]);
+            Assert.AreEqual("m_uut_AboutToAddItems System.String[] | m_uut_AddedItems System.String[] | m_uut_ContentsChanged | m_uut_AboutToReplaceItem Mary with Steve | m_uut_ReplacedItem Mary with Steve | m_uut_ContentsChanged | ", _responses);
         }
 
         [TestMethod]
@@ -181,39 +208,40 @@ namespace SageTestLib {
         {
             Init();
 
-            m_uut.AddRange(new string[] {"Bob", "Mary", "Sue"});
+            _uut.AddRange(new string[] { "Bob", "Mary", "Sue" });
 
-            m_uut.Insert(1, "Paul");
+            _uut.Insert(1, "Paul");
 
-            Assert.AreEqual("Bob", m_uut[0]);
-            Assert.AreEqual("Paul", m_uut[1]);
-            Assert.AreEqual("Mary", m_uut[2]);
-            Assert.AreEqual("Sue", m_uut[3]);
+            Assert.AreEqual("Bob", _uut[0]);
+            Assert.AreEqual("Paul", _uut[1]);
+            Assert.AreEqual("Mary", _uut[2]);
+            Assert.AreEqual("Sue", _uut[3]);
             Assert.AreEqual(
                 "m_uut_AboutToAddItems System.String[] | m_uut_AddedItems System.String[] | m_uut_ContentsChanged | m_uut_AboutToAddItem Paul | m_uut_AddedItem Paul | m_uut_ContentsChanged | ",
-                m_responses
+                _responses
                 );
         }
 
         [TestMethod]
         [Highpoint.Sage.Utility.FieldDescription("Test the InsertRange mechanism.")]
-        public void TestInsertRange() {
+        public void TestInsertRange()
+        {
             Init();
 
-            m_uut.AddRange(new string[] { "Bob", "Mary", "Tim" });
+            _uut.AddRange(new string[] { "Bob", "Mary", "Tim" });
 
-            m_uut.InsertRange(1, new string[] { "Paul", "Randy", "Sara" });
+            _uut.InsertRange(1, new string[] { "Paul", "Randy", "Sara" });
 
-            Assert.IsTrue(m_uut[0].Equals("Bob"));
-            Assert.IsTrue(m_uut[1].Equals("Paul"));
-            Assert.IsTrue(m_uut[2].Equals("Randy"));
-            Assert.IsTrue(m_uut[3].Equals("Sara"));
-            Assert.IsTrue(m_uut[4].Equals("Mary"));
-            Assert.IsTrue(m_uut[5].Equals("Tim"));
-            Assert.IsTrue(m_responses.Equals("m_uut_AboutToAddItems System.String[] | m_uut_AddedItems System.String[] | m_uut_ContentsChanged | m_uut_AboutToAddItems System.String[] | m_uut_AddedItems System.String[] | m_uut_ContentsChanged | "));
-            Console.WriteLine(m_responses);
+            Assert.IsTrue(_uut[0].Equals("Bob"));
+            Assert.IsTrue(_uut[1].Equals("Paul"));
+            Assert.IsTrue(_uut[2].Equals("Randy"));
+            Assert.IsTrue(_uut[3].Equals("Sara"));
+            Assert.IsTrue(_uut[4].Equals("Mary"));
+            Assert.IsTrue(_uut[5].Equals("Tim"));
+            Assert.IsTrue(_responses.Equals("m_uut_AboutToAddItems System.String[] | m_uut_AddedItems System.String[] | m_uut_ContentsChanged | m_uut_AboutToAddItems System.String[] | m_uut_AddedItems System.String[] | m_uut_ContentsChanged | "));
+            Console.WriteLine(_responses);
         }
-        
+
 
     }
 }
