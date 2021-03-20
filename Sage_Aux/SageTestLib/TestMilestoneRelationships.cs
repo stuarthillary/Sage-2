a8c2e628dc096f9c932934daddbc721b9d6f9318
stuarthillary <shillary.git@gmail.com>
2021-03-20 01:48:36 +0000

    TestMilestoneRelationship cleanup

--- Sage_Aux/SageTestLib/TestMilestoneRelationships.cs
+++ Sage_Aux/SageTestLib/TestMilestoneRelationships.cs
@@ -1,78 +1,85 @@
 /* This source code licensed under the GNU Affero General Public License */
 
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Diagnostics;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 
-namespace Highpoint.Sage.Scheduling {
+namespace Highpoint.Sage.Scheduling
+{
 
-	/// <summary>
-	/// Summary description for zTestTimePeriods.
-	/// </summary>
-	[TestClass]
-	public class MilestoneRelationshipTester {
-		public MilestoneRelationshipTester() {
-			Now = DateTime.Now;
-			FiveMinutes    = TimeSpan.FromMinutes(5);
-			TenMinutes     = TimeSpan.FromMinutes(10);
-			FifteenMinutes = TimeSpan.FromMinutes(15);
-			TwentyMinutes  = TimeSpan.FromMinutes(20);
-			FiveMinsAgo    = Now - FiveMinutes;
-			FiveMinsOn     = Now + FiveMinutes;
-			TenMinsAgo     = Now - TenMinutes;
-			TenMinsOn      = Now + TenMinutes;
-			TwentyMinsOn   = Now + TwentyMinutes;
-			TwentyMinsAgo  = Now - TwentyMinutes;
-		}
+    /// <summary>
+    /// Summary description for zTestTimePeriods.
+    /// </summary>
+    [TestClass]
+    public class MilestoneRelationshipTester
+    {
+        public MilestoneRelationshipTester()
+        {
+            _now = DateTime.Now;
+            _fiveMinutes = TimeSpan.FromMinutes(5);
+            _tenMinutes = TimeSpan.FromMinutes(10);
+            _fifteenMinutes = TimeSpan.FromMinutes(15);
+            _twentyMinutes = TimeSpan.FromMinutes(20);
+            _fiveMinsAgo = _now - _fiveMinutes;
+            _fiveMinsOn = _now + _fiveMinutes;
+            _tenMinsAgo = _now - _tenMinutes;
+            _tenMinsOn = _now + _tenMinutes;
+            _twentyMinsOn = _now + _twentyMinutes;
+            _twentyMinsAgo = _now - _twentyMinutes;
+        }
 
-		private DateTime Now = new DateTime(2001,05,16,12,0,0);
-		private TimeSpan FiveMinutes;
-		private TimeSpan TenMinutes;
-		private TimeSpan FifteenMinutes;
-		private TimeSpan TwentyMinutes;
-		private DateTime FiveMinsAgo;
-		private DateTime FiveMinsOn;
-		private DateTime TenMinsAgo;
-		private DateTime TenMinsOn;
-		private DateTime TwentyMinsOn;
-		private DateTime TwentyMinsAgo;
+        private readonly DateTime _now = new DateTime(2001, 05, 16, 12, 0, 0);
+        private readonly TimeSpan _fiveMinutes;
+        private readonly TimeSpan _tenMinutes;
+        private readonly TimeSpan _fifteenMinutes;
+        private readonly TimeSpan _twentyMinutes;
+        private readonly DateTime _fiveMinsAgo;
+        private readonly DateTime _fiveMinsOn;
+        private readonly DateTime _tenMinsAgo;
+        private readonly DateTime _tenMinsOn;
+        private readonly DateTime _twentyMinsOn;
+        private readonly DateTime _twentyMinsAgo;
 
-		[TestInitialize] 
-		public void Init() {
-		}
-		[TestCleanup]
-		public void destroy() {
-			Debug.WriteLine( "Done." );
-		}
+        [TestInitialize]
+        public void Init()
+        {
+        }
+        [TestCleanup]
+        public void destroy()
+        {
+            Debug.WriteLine("Done.");
+        }
 
-		[TestMethod]
-		public void TestMilestones(){
-			Milestone ms1 = new Milestone(DateTime.Now);
-			ms1.ChangeEvent +=new ObservableChangeHandler(ChangeEvent);
-			Debug.WriteLine(ms1.ToString());
-			ms1.MoveBy(-TwentyMinutes);
-			Debug.WriteLine(ms1.ToString());
+        [TestMethod]
+        public void TestMilestones()
+        {
+            Milestone ms1 = new Milestone(DateTime.Now);
+            ms1.ChangeEvent += new ObservableChangeHandler(ChangeEvent);
+            Debug.WriteLine(ms1.ToString());
+            ms1.MoveBy(-_twentyMinutes);
+            Debug.WriteLine(ms1.ToString());
 
-			Milestone ms2 = new Milestone(DateTime.Now+FiveMinutes);
-			ms2.ChangeEvent +=new ObservableChangeHandler(ChangeEvent);
-			Debug.WriteLine(ms2.ToString());
-			ms2.MoveBy(-TwentyMinutes);
-			Debug.WriteLine(ms2.ToString());
+            Milestone ms2 = new Milestone(DateTime.Now + _fiveMinutes);
+            ms2.ChangeEvent += new ObservableChangeHandler(ChangeEvent);
+            Debug.WriteLine(ms2.ToString());
+            ms2.MoveBy(-_twentyMinutes);
+            Debug.WriteLine(ms2.ToString());
 
-            Debug.WriteLine("Milestone 1 is at " + ms1 +", and Milestone 2 is at " + ms2 +". Strutting them together.");
-			MilestoneRelationship mr = new MilestoneRelationship_Strut(ms1,ms2);
-			ms1.AddRelationship(mr);
-			ms2.AddRelationship(mr);
+            Debug.WriteLine("Milestone 1 is at " + ms1 + ", and Milestone 2 is at " + ms2 + ". Strutting them together.");
+            MilestoneRelationship mr = new MilestoneRelationship_Strut(ms1, ms2);
+            ms1.AddRelationship(mr);
+            ms2.AddRelationship(mr);
 
             Debug.WriteLine("Moving Milestone1 by ten minutes.");
-			ms1.MoveBy(TenMinutes);
+            ms1.MoveBy(_tenMinutes);
             Debug.WriteLine("Milestone 1 is at " + ms1 + ", and Milestone 2 is at " + ms2 + ".");
 
-		}
+        }
 
-		private void ChangeEvent(object whoChanged, object whatChanged, object howChanged) {
-			Debug.WriteLine(((Milestone)whoChanged).ToString() + " changed by " + howChanged.ToString());
-		}
-	}
+        private void ChangeEvent(object whoChanged, object whatChanged, object howChanged)
+        {
+            Debug.WriteLine(((Milestone)whoChanged).ToString() + " changed by " + howChanged.ToString());
+        }
+    }
 }
